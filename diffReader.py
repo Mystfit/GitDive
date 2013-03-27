1ce5c855519fe150dd07da7f76562a201b1fc782
Byron Mallett <byronated@gmail.com>
2013-03-27 19:56:01 +1300

    032713195601--Gitdive autolog

--- diffReader.py
+++ diffReader.py
@@ -61,13 +61,13 @@ class CommitEntry:
 		self.diffList.append(self.parseRawDiff(diff))
 
 	def serializeCommit(self):
-		commitText = "***=== Commit Start ===***\n"
+		commitText = "///***=== Commit Start ===***\\\\\\\n"
 		commitText += "== Commit hash: " + self.commitHash + "\n"
 		commitText += "== Author: " + self.author + "\n"
 		commitText += "== Date: " + self.date + "\n"
 		commitText += "== Message: " + self.message + "\n\n"
 		commitText += self.serializeDiffs()
-		commitText += "***=== Commit Finish ===***\n\n\n"
+		commitText += "\\\\\\***=== Commit Finish ===***///\n\n\n"
 		return commitText
 
 	# Convert diffs into a readable string
@@ -75,15 +75,15 @@ class CommitEntry:
 		hunkString = ""
 
 		for i in range(len(self.diffList[0])):
-			hunkString += "=== Diff Start ===\n"
+			hunkString += "//=== Diff Start ===\\\\\n"
 			diff = self.diffList[0][i]
 
 			for j in range(len(diff.hunkList)):
-				hunkString += "--- Hunk Start ---\n"
+				hunkString += "/--- Hunk Start ---\\\n"
 				for k in range(len(diff.hunkList[j].hunkData)):
 					hunkString += diff.hunkList[j].hunkData[k]
-				hunkString += "--- Hunk Finish ---\n"
-			hunkString += "=== Diff Finish ===\n\n"
+				hunkString += "\\--- Hunk Finish ---/\n"
+			hunkString += "\\\\=== Diff Finish ===//\n\n"
 
 
 		return hunkString + "\n"
