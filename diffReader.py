55d876fabab4f3c1faf20b4992731ee8df7c3516
Byron Mallett <byronated@gmail.com>
2013-03-27 19:57:26 +1300

    032713195726--Gitdive autolog

--- diffReader.py
+++ diffReader.py
@@ -75,15 +75,15 @@ class CommitEntry:
 		hunkString = ""
 
 		for i in range(len(self.diffList[0])):
-			hunkString += "/=== Diff Start ===\\\n"
+			hunkString += "  /=== Diff Start ===\\\n"
 			diff = self.diffList[0][i]
 
 			for j in range(len(diff.hunkList)):
-				hunkString += "/--- Hunk Start ---\\\n"
+				hunkString += "    /--- Hunk Start ---\\\n"
 				for k in range(len(diff.hunkList[j].hunkData)):
 					hunkString += diff.hunkList[j].hunkData[k]
-				hunkString += "\\--- Hunk Finish ---/\n"
-			hunkString += "\\=== Diff Finish ===/\n\n"
+				hunkString += "    \\--- Hunk Finish ---/\n"
+			hunkString += "  \\=== Diff Finish ===/\n\n"
 
 
 		return hunkString + "\n"
