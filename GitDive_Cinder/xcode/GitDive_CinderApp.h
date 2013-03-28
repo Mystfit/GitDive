2c68ff61b2aea574a4ee103605a33b8b25d65230
Byron Mallett <byronated@gmail.com>
2013-03-28 20:46:18 +1300

    Gitdive_autolog | MOOD:"tired" | COFFEE:none | LOCATION:"masters couch"

---
+++ GitDive_Cinder/xcode/GitDive_CinderApp.h
@@ -0,0 +1,14 @@
+//
+//  GitDive_CinderApp.h
+//  GitDive_Cinder
+//
+//  Created by Byron Mallett on 28/03/13.
+//
+//
+
+#ifndef GitDive_Cinder_GitDive_CinderApp_h
+#define GitDive_Cinder_GitDive_CinderApp_h
+
+
+
+#endif
