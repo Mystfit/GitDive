6fc898be7be9ac6822fefaa3bea6a04ac1de78ed
Byron Mallett <byronated@gmail.com>
2013-03-29 17:47:17 +1300

    Gitdive_autolog | MOOD:"chirpy" | COFFEE:afogato | LOCATION:"home"

--- GitDive_Cinder/include/Utils.h
+++ GitDive_Cinder/include/Utils.h
@@ -19,7 +19,7 @@ using namespace std;
 class Utils {
 public:
     
-    typedef boost::iostreams::<boost::iostreams::file_descriptor_sink> boost_stream;
+    typedef boost::iostreams::filt<boost::iostreams::file_descriptor_sink> boost_stream;
     
     string getCmdOutput()
     {
