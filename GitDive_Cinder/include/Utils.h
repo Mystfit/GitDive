5f072587dc00f9a763bd5d330713d1a7ad9cee4f
Byron Mallett <byronated@gmail.com>
2013-03-29 20:59:34 +1300

    Gitdive_autolog | MOOD:"frustrated" | COFFEE:none | LOCATION:"home"

--- GitDive_Cinder/include/Utils.h
+++ GitDive_Cinder/include/Utils.h
@@ -23,15 +23,14 @@ public:
     {
         FILE *cmdStream = popen(command, "r");
         
-
         if (!cmdStream) return "Command failed";
-        
        
         io::file_descriptor_source d(fileno(cmdStream), io::close_handle);
         
         io::stream_buffer<io::file_descriptor_source> pstream(d);
         cout << &pstream;
         pclose(cmdStream);
+        
         
         string cmdResult = "End";
              
