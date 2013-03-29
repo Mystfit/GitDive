8536cb0870b0d3c2c9e67b98b6b8473e331726fb
Byron Mallett <byronated@gmail.com>
2013-03-29 23:31:30 +1300

    Gitdive_autolog | MOOD:"weekend" | COFFEE:none | LOCATION:"home"

--- GitDive_Cinder/src/Utils.cpp
+++ GitDive_Cinder/src/Utils.cpp
@@ -7,3 +7,28 @@
 //
 
 #include "Utils.h"
+
+static string Utils::getCmdOutput(const char* command)
+{
+    FILE *cmdStream = popen(command, "r");
+    
+    if (!cmdStream) return "Command failed";
+    
+    io::file_descriptor_source d(fileno(cmdStream), io::close_handle);
+    
+    io::stream_buffer<io::file_descriptor_source> pstream(d);
+    istream in(&pstream);
+    
+    //cout << &pstream;
+    string cmdResult = "";
+    string line;
+    
+    while(in){
+        getline(in, line);
+        cmdResult += line + "\n";
+    }
+    
+    pclose(cmdStream);
+    
+    return cmdResult;
+}
