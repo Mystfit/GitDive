57c50d0cb33166b703a566af29e56bbfc8040d90
Byron Mallett <byronated@gmail.com>
2013-03-29 21:14:22 +1300

    Gitdive_autolog | MOOD:"ecstatic" | COFFEE:none | LOCATION:"home"

--- GitDive_Cinder/src/GitDive_CinderApp.cpp
+++ GitDive_Cinder/src/GitDive_CinderApp.cpp
@@ -4,7 +4,7 @@
 
 void GitDive_CinderApp::setup()
 {
-    Utils::getCmdOutput("echo 'hello\nline test\nanotherline' >> test.txt");
+    Utils::getCmdOutput("echo 'hello\nline test\nanotherline' >> test.txt; cat test.txt");
 }
 
 void GitDive_CinderApp::mouseDown( MouseEvent event )
