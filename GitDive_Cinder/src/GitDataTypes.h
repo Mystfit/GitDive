a7a5db9d42f56c51d74dcf9fc92a5c050cc128f8
Byron Mallett <byronated@gmail.com>
2013-03-30 00:29:37 +1300

    Gitdive_autolog | MOOD:"weekend" | COFFEE:none | LOCATION:"home"

--- GitDive_Cinder/src/GitDataTypes.h
+++ GitDive_Cinder/src/GitDataTypes.h
@@ -23,10 +23,12 @@ public:
         m_startAddLines = linePos;
         m_numAddLines = numLines;
     };
+    
     void setPostChangedLines(int linePos, int numLines){
         m_startRemoveLines = linePos;
         m_numRemoveLines = numLines;
     }
+    
 private:
     string m_startRemoveLines;
     string m_numRemoveLines;
