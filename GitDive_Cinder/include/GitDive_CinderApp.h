3fe8f41b3648a10453a270ff389bc84d69ddf355
Byron Mallett <byronated@gmail.com>
2013-03-29 17:19:22 +1300

    Gitdive_autolog | MOOD:"tired" | COFFEE:none | LOCATION:"masters couch"

---
+++ GitDive_Cinder/include/GitDive_CinderApp.h
@@ -0,0 +1,30 @@
+//
+//  GitDive_CinderApp.h
+//  GitDive_Cinder
+//
+//  Created by Byron Mallett on 28/03/13.
+//
+//
+
+#ifndef GitDive_Cinder_GitDive_CinderApp_h
+#define GitDive_Cinder_GitDive_CinderApp_h
+
+#include "cinder/app/AppBasic.h"
+#include "cinder/gl/gl.h"
+
+#include <stdio.h>
+
+
+using namespace ci;
+using namespace ci::app;
+using namespace std;
+
+class GitDive_CinderApp : public AppBasic {
+public:
+	void setup();
+	void mouseDown( MouseEvent event );
+	void update();
+	void draw();
+};
+
+#endif
