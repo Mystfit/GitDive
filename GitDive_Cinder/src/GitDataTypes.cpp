f180bfdfe099a1f0edf84e32fdffe5cfd5f484a8
Byron Mallett <byronated@gmail.com>
2013-03-29 23:47:51 +1300

    Gitdive_autolog | MOOD:"weekend" | COFFEE:none | LOCATION:"home"

--- GitDive_Cinder/src/GitDataTypes.cpp
+++ GitDive_Cinder/src/GitDataTypes.cpp
@@ -12,3 +12,11 @@
 Commit::Commit(string commit, string date, string author, string message){
     
 }
+
+Diff::Diff(){
+    
+}
+
+Commit::Commit(string commit, string date, string author, string message){
+    
+}
