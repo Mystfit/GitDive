e5ddcddc3e869d3ed3c014b7edf3b3e18121c36e
Byron Mallett <byronated@gmail.com>
2013-03-29 18:13:37 +1300

    Gitdive_autolog | MOOD:"chirpy" | COFFEE:afogato | LOCATION:"home"

--- GitDive_Cinder/include/Utils.h
+++ GitDive_Cinder/include/Utils.h
@@ -30,7 +30,7 @@ public:
         if (!cmdStream) return "Command failed";
         
         
-        boost::iostreams::file_descriptor_source p2(fileno(cmdStream));
+        boost_stream p2(fileno(cmdStream));
         
         string s;
         p2 >> s;
