326841c38f2858349cf9c82e32b5a0428abc0311
Byron Mallett <byronated@gmail.com>
2013-03-29 23:33:44 +1300

    Gitdive_autolog | MOOD:"weekend" | COFFEE:none | LOCATION:"home"

---
+++ GitDive_Cinder/src/GitDataTypes.cpp
@@ -0,0 +1,9 @@
+//
+//  GitDataTypes.cpp
+//  GitDive_Cinder
+//
+//  Created by Byron Mallett on 29/03/13.
+//
+//
+
+#include "GitDataTypes.h"
---
+++ GitDive_Cinder/src/GitDataTypes.h
@@ -0,0 +1,14 @@
+//
+//  GitDataTypes.h
+//  GitDive_Cinder
+//
+//  Created by Byron Mallett on 29/03/13.
+//
+//
+
+#ifndef __GitDive_Cinder__GitDataTypes__
+#define __GitDive_Cinder__GitDataTypes__
+
+#include <iostream>
+
+#endif /* defined(__GitDive_Cinder__GitDataTypes__) */
