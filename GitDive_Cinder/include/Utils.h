8886e8019dc2bc39f76d7dd6cf52d269caff93da
Byron Mallett <byronated@gmail.com>
2013-03-29 17:21:59 +1300

    Gitdive_autolog | MOOD:"tired" | COFFEE:none | LOCATION:"masters couch"

--- GitDive_Cinder/include/Utils.h
+++ GitDive_Cinder/include/Utils.h
@@ -11,6 +11,20 @@
 
 #include <iostream>
 
-
+class Utils {
+public:
+    string getCmdOutput()
+    {
+        typedef boost::iostreams::stream<boost::iostreams::file_descriptor_sink>
+        boost_stream;
+        
+        FILE *myfile;
+        // make sure to popen and it succeeds
+        boost_stream stream(fileno(myfile));
+        stream.set_auto_close(false); // https://svn.boost.org/trac/boost/ticket/3517
+        std::string mystring;
+        while(std::getline(stream,mystring))
+    }
+};
 
 #endif /* defined(__GitDive_Cinder__Utils__) */
