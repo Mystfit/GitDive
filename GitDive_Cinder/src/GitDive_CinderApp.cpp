2e4d142f698b7422abc04a1fda4c27e9cd7adb00
Byron Mallett <byronated@gmail.com>
2013-03-29 23:26:44 +1300

    Gitdive_autolog | MOOD:"ecstatic" | COFFEE:none | LOCATION:"home"

--- GitDive_Cinder/src/GitDive_CinderApp.cpp
+++ GitDive_Cinder/src/GitDive_CinderApp.cpp
@@ -6,8 +6,9 @@ void GitDive_CinderApp::setup()
 {
     string repoPath = "/Users/mystfit/Code/Python/Gitdive_Prototypes";
     string gitCmd = "git log --reverse --pretty=format:\"GD_commit|-%H||GD_commitAuthor|-%cn||GD_date|-%cd||GD_message|-%B\"";
+    string combinedCmd = repoPath + " && " + gitCmd;
     
-    console() << Utils::getCmdOutput(repoPath + " && " + gitCmd) << endl;
+    console() << Utils::getCmdOutput(combinedCmd) << endl;
 }
 
 void GitDive_CinderApp::mouseDown( MouseEvent event )
