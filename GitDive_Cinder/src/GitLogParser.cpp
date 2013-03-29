e06c1a962b590195c5a26be4bff26175770c2893
Byron Mallett <byronated@gmail.com>
2013-03-29 23:56:38 +1300

    Gitdive_autolog | MOOD:"weekend" | COFFEE:none | LOCATION:"home"

---
+++ GitDive_Cinder/src/GitLogParser.cpp
@@ -0,0 +1,9 @@
+//
+//  GitLogParser.cpp
+//  GitDive_Cinder
+//
+//  Created by Byron Mallett on 29/03/13.
+//
+//
+
+#include "GitLogParser.h"
---
+++ GitDive_Cinder/src/GitLogParser.h
@@ -0,0 +1,14 @@
+//
+//  GitLogParser.h
+//  GitDive_Cinder
+//
+//  Created by Byron Mallett on 29/03/13.
+//
+//
+
+#ifndef __GitDive_Cinder__GitLogParser__
+#define __GitDive_Cinder__GitLogParser__
+
+#include <iostream>
+
+#endif /* defined(__GitDive_Cinder__GitLogParser__) */
