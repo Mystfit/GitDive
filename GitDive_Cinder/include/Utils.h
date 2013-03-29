490f111758b95be5ea20ad51fe14aa4848b28e1d
Byron Mallett <byronated@gmail.com>
2013-03-29 23:31:43 +1300

    Gitdive_autolog | MOOD:"weekend" | COFFEE:none | LOCATION:"home"

--- GitDive_Cinder/include/Utils.h
+++ GitDive_Cinder/include/Utils.h
@@ -19,30 +19,7 @@ using namespace std;
 
 class Utils {
 public:
-    static string getCmdOutput(const char* command)
-    {
-        FILE *cmdStream = popen(command, "r");
-        
-        if (!cmdStream) return "Command failed";
-       
-        io::file_descriptor_source d(fileno(cmdStream), io::close_handle);
-        
-        io::stream_buffer<io::file_descriptor_source> pstream(d);
-        istream in(&pstream);
-
-        //cout << &pstream;
-        string cmdResult = "";
-        string line;
-        
-        while(in){
-            getline(in, line);
-            cmdResult += line + "\n";
-        }
-
-        pclose(cmdStream);
-                
-        return cmdResult;
-    }
+    static string getCmdOutput(const char* command);
 };
 
 #endif /* defined(__GitDive_Cinder__Utils__) */
