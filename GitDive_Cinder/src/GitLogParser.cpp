4c052f62daff8e8a0baefc834aac4b5b517eac0d
Byron Mallett <byronated@gmail.com>
2013-03-31 02:43:22 +1300

    Gitdive_autolog | MOOD:"tired" | COFFEE:none | LOCATION:"home"

--- GitDive_Cinder/src/GitLogParser.cpp
+++ GitDive_Cinder/src/GitLogParser.cpp
@@ -105,7 +105,7 @@ vector< shared_ptr<Diff> > GitLogParser::parseCommit(vector<string> diffBlock)
             //Start a new hunk in the current diff
             if(starts_with(line, "@@ "))
             {
-
+                //Creates a new diffHunk to store changed lines
                 shared_ptr<DiffHunk> diffHunkPtr;
                 diffHunk = diffHunkPtr;
                 diff->addDiffHunk(diffHunk);
