820fb731579284505a1ec8770ee734617c76b3f3
Byron Mallett <byronated@gmail.com>
2013-03-31 02:24:43 +1300

    Gitdive_autolog | MOOD:"tired" | COFFEE:none | LOCATION:"home"

--- GitDive_Cinder/src/GitLogParser.cpp
+++ GitDive_Cinder/src/GitLogParser.cpp
@@ -55,7 +55,7 @@ vector< shared_ptr<Diff> GitLogParser::parseCommit(vector<string> diffBlock)
     bool inDiffHeader = false;
     vector< shared_ptr<Diff> > diffList;
     shared_ptr<Diff> diff;
-    DiffHunk diffHunk;
+    shared_ptr<DiffHunk> diffHunk;
     
     for(int i = 0; i < diffBlock.size(); i++){
         
