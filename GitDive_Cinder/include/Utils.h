a319f2ae6314687ca63f77c32a709bea1f310cea
Byron Mallett <byronated@gmail.com>
2013-03-30 19:59:25 +1300

    Gitdive_autolog | MOOD:"hungry" | COFFEE:none | LOCATION:"home"

--- GitDive_Cinder/include/Utils.h
+++ GitDive_Cinder/include/Utils.h
@@ -23,7 +23,7 @@ class Utils {
 public:
     //Returns stdout from a given command
     static string getCmdOutput(const char* command);
-    static vector<string> splitTextLines(string input){     boost::split(lines,input,boost::is_any_of("\n")); }
+    static vector<string> splitTextLines(string input){ boost::split(lines,input,boost::is_any_of("\n")); }
     static void strip(string &input, string search){ boost::replace_all(input, search, ""); }
 
 };
