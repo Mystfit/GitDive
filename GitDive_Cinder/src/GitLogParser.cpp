8c53dc5d0eee3a5fd0ac843180192d3d72a9669c
Byron Mallett <byronated@gmail.com>
2013-03-31 02:50:11 +1300

    Gitdive_autolog | MOOD:"tired" | COFFEE:none | LOCATION:"home"

--- GitDive_Cinder/src/GitLogParser.cpp
+++ GitDive_Cinder/src/GitLogParser.cpp
@@ -110,11 +110,8 @@ vector< shared_ptr<Diff> > GitLogParser::parseCommit(vector<string> diffBlock)
                 diffHunk = diffHunkPtr;
                 diff->addDiffHunk(diffHunk);
                 
-                
                 vector<string> splitLine;
-                
                 split(splitLine, line, is_any_of(" ,"));
-                
             
                 diffHunk->setPreChangedLines(atoi(splitLine[1].c_str()), atoi(splitLine[2].c_str()));
                 diffHunk->setPostChangedLines(atoi(splitLine[3].c_str()), atoi(splitLine[4].c_str()));
