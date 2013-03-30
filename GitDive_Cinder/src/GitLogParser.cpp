9b498ea6d7e149e25c678cfe4ba2901286f762cd
Byron Mallett <byronated@gmail.com>
2013-03-31 02:27:25 +1300

    Gitdive_autolog | MOOD:"tired" | COFFEE:none | LOCATION:"home"

--- GitDive_Cinder/src/GitLogParser.cpp
+++ GitDive_Cinder/src/GitLogParser.cpp
@@ -67,7 +67,7 @@ vector< shared_ptr<Diff> GitLogParser::parseCommit(vector<string> diffBlock)
             
             //Add last diff to the list
             if(i > 0) diffList.push_back(diff);
-            diff = DIff();
+            diff = Diff();
             
             //Move through the header and set up the diff object from the parsed diff vars
             if(inDiffHeader){
