9b8e072cc76797cc3bf4d9fb8b827988b1341173
Byron Mallett <byronated@gmail.com>
2013-03-30 23:09:33 +1300

    Gitdive_autolog | MOOD:"hungry" | COFFEE:none | LOCATION:"home"

--- GitDive_Cinder/src/GitLogParser.cpp
+++ GitDive_Cinder/src/GitLogParser.cpp
@@ -35,7 +35,8 @@ vector<Commit> GitLogParser::parseLog(vector<string> diffLog)
             }
             
             if(commitList.size() > 0){
-                commitList[commitList.size()-1].addDiff(<#Diff diff#>)
+                Diff diff;
+                commitList[commitList.size()-1].addDiff()
             }
             
             commitList.push_back( Commit(strVars[0], strVars[1], strVars[2], strVars[3]) );
