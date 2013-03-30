07b08055a4dc4f5f1e2d601579cd3e68522a7b6f
Byron Mallett <byronated@gmail.com>
2013-03-30 19:20:33 +1300

    Gitdive_autolog | MOOD:"weekend" | COFFEE:none | LOCATION:"home"

--- GitDive_Cinder/include/Utils.h
+++ GitDive_Cinder/include/Utils.h
@@ -21,7 +21,7 @@ class Utils {
 public:
     //Returns stdout from a given command
     static string getCmdOutput(const char* command);
-    static string splitTextLines(string input, string &dest);
+    static ve splitTextLines(string input, string &dest);
 };
 
 #endif /* defined(__GitDive_Cinder__Utils__) */
