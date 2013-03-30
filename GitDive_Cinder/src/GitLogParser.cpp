c555903cefc4b2d98368c3c898b693706b2dc3b4
Byron Mallett <byronated@gmail.com>
2013-03-31 01:58:57 +1300

    Gitdive_autolog | MOOD:"hungry" | COFFEE:none | LOCATION:"home"

--- GitDive_Cinder/src/GitLogParser.cpp
+++ GitDive_Cinder/src/GitLogParser.cpp
@@ -106,6 +106,7 @@ vector<Diff> GitLogParser::parseCommit(vector<string> diffBlock)
                 }
             }
             
+            //
             if(boost::starts_with(line, "@@ "))
             {
                 inDiffHeader = false;
