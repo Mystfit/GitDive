b39c870ff5437a02e62977cca75b484ce338c847
Byron Mallett <byronated@gmail.com>
2013-03-31 02:39:56 +1300

    Gitdive_autolog | MOOD:"tired" | COFFEE:none | LOCATION:"home"

--- GitDive_Cinder/src/GitLogParser.cpp
+++ GitDive_Cinder/src/GitLogParser.cpp
@@ -109,7 +109,7 @@ vector< shared_ptr<Diff> > GitLogParser::parseCommit(vector<string> diffBlock)
             {
                 shared_ptr<DiffHunk> diffHunkPtr;
                 diffHunk = diffHunkPtr;
-                diffList.push_back(diff);
+                diffList.push_back(diffHunk);
                 
                 inDiffHeader = false;
             }
