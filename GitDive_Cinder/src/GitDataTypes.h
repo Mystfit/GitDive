a438d84603c3ce96a4fcd60cbc3960da7d3fc775
Byron Mallett <byronated@gmail.com>
2013-03-31 02:34:29 +1300

    Gitdive_autolog | MOOD:"tired" | COFFEE:none | LOCATION:"home"

--- GitDive_Cinder/src/GitDataTypes.h
+++ GitDive_Cinder/src/GitDataTypes.h
@@ -67,7 +67,7 @@ public:
     string date;
     string message;
     
-    void addDiff(shared_ptr<Diff> diff){ m_diffList.push_back(diff); };
+    void addDiff(boost::shared_ptr<Diff> diff){ m_diffList.push_back(diff); };
     void addDiffList(vector<Diff> diffList){ for(int i = 0; i < diffList.size(); i++) m_diffList.push_back(diffList[i]); }
 
 
