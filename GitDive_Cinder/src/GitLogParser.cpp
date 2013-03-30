bc02cf57399337d61418b337acba9082a72ccc34
Byron Mallett <byronated@gmail.com>
2013-03-30 20:26:49 +1300

    Gitdive_autolog | MOOD:"hungry" | COFFEE:none | LOCATION:"home"

--- GitDive_Cinder/src/GitLogParser.cpp
+++ GitDive_Cinder/src/GitLogParser.cpp
@@ -25,7 +25,7 @@ vector<Commit> GitLogParser::parseLog(vector<string> diffLog)
             
             
             vector<string> hashStrings;
-            boost::split(hashStrings, logVars[, boost::is_any_of("||"));
+            boost::split(hashStrings, logVars[0], boost::is_any_of("||"));
 
             
             for(int j = 0; j < logVars.size(); j++){
