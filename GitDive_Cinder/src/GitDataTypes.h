ae31d277ad4aec4d91c6a9018c2f7f9ea0e55ff9
Byron Mallett <byronated@gmail.com>
2013-03-31 15:36:48 +1300

    Gitdive_autolog | MOOD:"tired" | COFFEE:none | LOCATION:"home"

--- GitDive_Cinder/src/GitDataTypes.h
+++ GitDive_Cinder/src/GitDataTypes.h
@@ -58,7 +58,7 @@ public:
     void setFileNameB(string filename){ m_fileNameB = filename; };
     
     string getFileName(){
-        if(m_fileNameA != ")
+        if(m_fileNameA != "/dev/null") return 
     }
     
     int getNumHunks(){ return m_hunkList.size(); };
