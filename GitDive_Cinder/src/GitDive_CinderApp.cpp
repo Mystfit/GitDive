a76592ad923d7133db542d03b30ccaf0c7ae1607
Byron Mallett <byronated@gmail.com>
2013-03-31 18:42:41 +1300

    Gitdive_autolog | MOOD:"tired" | COFFEE:none | LOCATION:"home"

--- GitDive_Cinder/src/GitDive_CinderApp.cpp
+++ GitDive_Cinder/src/GitDive_CinderApp.cpp
@@ -21,7 +21,7 @@ void GitDive_CinderApp::setup()
     //Test of the commit serializer
     ofstream diffOutput;
     
-    diffOutput.open ("/Users/mystfit/desktop/diffOut.txt");
+    diffOutput.open ("/Users/mystfit/desktop/diffOut.log");
     for(int i = 0; i < commitList.size(); i++) {
         diffOutput << GitLogParser::serializeCommit(commitList[i]);
     }
