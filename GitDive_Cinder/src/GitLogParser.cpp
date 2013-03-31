e99ea42de70060598b21683cac21090764c67bbf
Byron Mallett <byronated@gmail.com>
2013-03-31 15:45:04 +1300

    Gitdive_autolog | MOOD:"tired" | COFFEE:none | LOCATION:"home"

--- GitDive_Cinder/src/GitLogParser.cpp
+++ GitDive_Cinder/src/GitLogParser.cpp
@@ -150,8 +150,8 @@ string GitLogParser::serializeCommit(shared_ptr<Commit> commit)
     commitText += "|== Date: " + commit->date + "\n";
     commitText += "|== Message: " + commit->message + "\n\n";
     
-    for(int i = 0; i < commit.getNumDiffs(); i++){
-        shared_ptr<Diff> diff = commit.getDiff(i);
+    for(int i = 0; i < commit->getNumDiffs(); i++){
+        shared_ptr<Diff> diff = commit->getDiff(i);
         commitText += "|/=== Diff File: " + diff->getFileName() + "\n";
         
         for(int j = 0; j < diff->getNumHunks(); j++){
