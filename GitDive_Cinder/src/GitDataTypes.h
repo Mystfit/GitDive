3cf1839dad60fddce26b13df7315e48b5f4172be
Byron Mallett <byronated@gmail.com>
2013-04-01 17:31:27 +1300

    Gitdive_autolog | MOOD:"super hungry" | COFFEE:none | LOCATION:"school"

--- GitDive_Cinder/src/GitDataTypes.h
+++ GitDive_Cinder/src/GitDataTypes.h
@@ -60,7 +60,6 @@ public:
     void addLine(Line line){ m_hunkData.push_back(line); };
     Line getLine(int index){ return m_hunkData[index]; };
     
-    
     void incrementLinePos(){ linePos++; };
     int linePos;
     
