04d3d1e9c20183109fe10de283fe82ebfe984906
Byron Mallett <byronated@gmail.com>
2013-04-01 17:54:18 +1300

    Gitdive_autolog | MOOD:"super hungry" | COFFEE:none | LOCATION:"school"

--- GitDive_Cinder/src/GitLogParser.cpp
+++ GitDive_Cinder/src/GitLogParser.cpp
@@ -136,10 +136,12 @@ vector< shared_ptr<Diff> > GitLogParser::parseCommit(vector<string> diffBlock)
                 
                 Line cLine(line);
                 
-                //If the line is an addition or unchanged, then we can move the hunk line position forwards
-                if(cLine.getLineState() == Line::LINE_ADDED || cLine.getLineState() == Line::LINE_NORMAL) diffHunk->incrementLinePos();
                 cLine.setLinePos(diffHunk->getLinepos());
                 diffHunk->addLine(cLine);
+                
+                //If the line is an addition or unchanged, then we can move the hunk line position forwards
+                if(cLine.getLineState() == Line::LINE_ADDED || cLine.getLineState() == Line::LINE_NORMAL) diffHunk->incrementLinePos();
+
             }
         }
                 
