23c4d527e9d4032d238c0626830b4a21d4a8d739
Byron Mallett <byronated@gmail.com>
2013-04-01 20:55:49 +1300

    Gitdive_autolog | MOOD:"super hungry" | COFFEE:none | LOCATION:"school"

---
+++ GitDive_Cinder/src/GitFileManager.cpp
@@ -0,0 +1,9 @@
+//
+//  GitFileManager.cpp
+//  GitDive_Cinder
+//
+//  Created by Byron Mallett on 1/04/13.
+//
+//
+
+#include "GitFileManager.h"
---
+++ GitDive_Cinder/src/GitFileManager.h
@@ -0,0 +1,14 @@
+//
+//  GitFileManager.h
+//  GitDive_Cinder
+//
+//  Created by Byron Mallett on 1/04/13.
+//
+//
+
+#ifndef __GitDive_Cinder__GitFileManager__
+#define __GitDive_Cinder__GitFileManager__
+
+#include <iostream>
+
+#endif /* defined(__GitDive_Cinder__GitFileManager__) */
