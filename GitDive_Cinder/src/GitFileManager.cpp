95358ce9fab8829cd6ab8b8788109ae798263675
Byron Mallett <byronated@gmail.com>
2013-04-01 21:18:24 +1300

    Gitdive_autolog | MOOD:"super hungry" | COFFEE:none | LOCATION:"school"

--- GitDive_Cinder/src/GitFileManager.cpp
+++ GitDive_Cinder/src/GitFileManager.cpp
@@ -12,6 +12,9 @@ GitFileManager::GitFileManager(){
     
 }
 
+void GitFileManager::addFile(GitFile file);
+
+
 boost::shared_ptr<GitFile> GitFileManager::getFileByName(string fileName){
     for(int i = 0; i < m_fileList.size(); i++){
         if(m_fileList[i]->filename == fileName) return m_fileList[i];
