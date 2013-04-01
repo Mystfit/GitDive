7cfc5869babbc0a625f75586a0faba6f8ea9df9c
Byron Mallett <byronated@gmail.com>
2013-04-01 18:04:18 +1300

    Gitdive_autolog | MOOD:"super hungry" | COFFEE:none | LOCATION:"school"

--- GitDive_Cinder/src/GitLogParser.cpp
+++ GitDive_Cinder/src/GitLogParser.cpp
@@ -137,7 +137,7 @@ vector< shared_ptr<Diff> > GitLogParser::parseCommit(vector<string> diffBlock)
                 
                 Line cLine(line);
                 
-                if(cLine.getLineState() == Line::LINE_ADDED) cLine.setLinePos(diffHunk->getLinepos());
+                if(cLine.getLineState() == Line::LINE_ADDED || cLine.getLineState() == Line::LINE_NORMAL ) cLine.setLinePos(diffHunk->getLinepos());
                 if(cLine.getLineState() == Line::LINE_DELETED) cLine.setLinePos(diffHunk->getOldLinepos());
 
                 diffHunk->addLine(cLine);
