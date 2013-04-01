29bf3cf3fde0ae895bbd2572cfb9345a039b36e5
Byron Mallett <byronated@gmail.com>
2013-04-01 16:42:27 +1300

    Gitdive_autolog | MOOD:"super hungry" | COFFEE:none | LOCATION:"school"

--- GitDive_Cinder/src/GitDataTypes.h
+++ GitDive_Cinder/src/GitDataTypes.h
@@ -26,7 +26,7 @@ public:
     
     Line(string lineStr);
     
-    string getLineStr(){return m_lineStr; };
+    string str(){return m_lineStr; };
     
     enum lineState{LINE_NORMAL = 0, LINE_ADDED, LINE_DELETED};
     void setLineState(int lineState){ m_lineState = lineState; };
