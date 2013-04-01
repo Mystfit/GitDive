166adb939153e15663a69fd6044561b756b6adc3
Byron Mallett <byronated@gmail.com>
2013-04-01 20:58:05 +1300

    Gitdive_autolog | MOOD:"super hungry" | COFFEE:none | LOCATION:"school"

---
+++ GitDive_Cinder/src/GitFile.h
@@ -0,0 +1,24 @@
+//
+//  GitFile.h
+//  GitDive_Cinder
+//
+//  Created by Byron Mallett on 1/04/13.
+//
+//
+
+#ifndef __GitDive_Cinder__GitFile__
+#define __GitDive_Cinder__GitFile__
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class GitFile {
+public:
+    GitFile();
+private:
+    vector<string> m_activeLines;
+};
+
+#endif /* defined(__GitDive_Cinder__GitFile__) */
