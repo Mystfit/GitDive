2de68eaae02c864bcb10245d625dee5ec4dfb7bb
Byron Mallett <byronated@gmail.com>
2013-04-02 14:45:31 +1300

    Gitdive_autolog | MOOD:"mellow" | COFFEE:chocolate | LOCATION:"masters couch"

--- GitDive_Cinder/src/GitFileManager.cpp
+++ GitDive_Cinder/src/GitFileManager.cpp
@@ -63,4 +63,6 @@ void GitFileManager::applyDiffToFile(boost::shared_ptr<GitFile> file, boost::sha
 }
 
 
-string GitFileManager::serializeFile(shared_ptr<File> file);
+string GitFileManager::serializeFile(shared_ptr<File> file){
+    
+}
