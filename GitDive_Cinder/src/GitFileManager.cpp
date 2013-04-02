e7dc9bf4337cc432f7c4ab22374f730b483d90da
Byron Mallett <byronated@gmail.com>
2013-04-02 13:17:15 +1300

    Gitdive_autolog | MOOD:"mellow" | COFFEE:lipton iced tea | LOCATION:"masters couch"

--- GitDive_Cinder/src/GitFileManager.cpp
+++ GitDive_Cinder/src/GitFileManager.cpp
@@ -44,7 +44,7 @@ void GitFileManager::applyDiffToFile(boost::shared_ptr<GitFile> file, boost::sha
             {
                 Line cLine = diffHunk->getLine(j);
                 
-                if(strcmp(cLine.getStr(), oldLine))
+                if(strcmp(cLine.getStr().c_str(), oldLine.c_str()))
             }
         }
         
