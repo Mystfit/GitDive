1742a62f3ff680dd6f01409f64968b2cf3cf95ef
Byron Mallett <byronated@gmail.com>
2013-04-02 20:48:24 +1300

    Gitdive_autolog | MOOD:"frustrated" | COFFEE:none | LOCATION:"masters desk"

--- GitDive_Cinder/src/GitFileManager.cpp
+++ GitDive_Cinder/src/GitFileManager.cpp
@@ -111,6 +111,7 @@ void GitFileManager::applyDiffToFile(boost::shared_ptr<GitFile> file, boost::sha
     deltaIndex = 0;
     
     cout << endl << "--Removed lines. Orig Size:" << originalLines.size() << " Num lines (minus removed):" << interimLines.size() << " Num lines(with added):" << interimLines.size() + deltaAddLines.size() << endl << endl;
+    cout << "--Lines to add:" << deltaAddLines.size() << endl;
     
     //Add lines second;
     if(deltaAddLines.size() < 1){
