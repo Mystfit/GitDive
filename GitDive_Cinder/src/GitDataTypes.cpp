bd2e3b35ecb210a31caf3684a7b1da3ec8713f67
Byron Mallett <byronated@gmail.com>
2013-04-02 13:30:31 +1300

    Gitdive_autolog | MOOD:"mellow" | COFFEE:lipton iced tea | LOCATION:"masters couch"

--- GitDive_Cinder/src/GitDataTypes.cpp
+++ GitDive_Cinder/src/GitDataTypes.cpp
@@ -54,7 +54,7 @@ int Diff::getDeltaNumLines(){
 
 boost::shared_ptr<Line> Diff::getAllDiffLines(){
     for(int i = 0; i < m_hunkList.size(); i++){
-        boost::shared_ptr<DiffHunk> diffHunk = getDiffHunk(i);
+        boost::shared_ptr<DiffHunk> diffHunk = m_hunkList[i]
         
         for(int j = 0; j < diffHunk->getNumLines(); j++)
         {
