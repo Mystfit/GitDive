62e9beff0980a892aeb3af0b10f31bfffbaf56c2
Byron Mallett <byronated@gmail.com>
2013-04-02 14:13:48 +1300

    Gitdive_autolog | MOOD:"mellow" | COFFEE:chocolate | LOCATION:"masters couch"

--- GitDive_Cinder/src/GitFileManager.cpp
+++ GitDive_Cinder/src/GitFileManager.cpp
@@ -48,7 +48,8 @@ void GitFileManager::applyDiffToFile(boost::shared_ptr<GitFile> file, boost::sha
         }
         
         //Always increment the old line position so we get the correct original line number
-        deltaLinePos++;
+        
+        linePos++;
         
         
         //IF Line is added -> DON'T increment oldLinePos, DO increment deltaLinePos
