2820fb2e73a1169e4bba0632dd08311ab1c812d0
Byron Mallett <byronated@gmail.com>
2013-04-02 17:26:29 +1300

    Gitdive_autolog | MOOD:"mellow" | COFFEE:none | LOCATION:"masters couch"

--- GitDive_Cinder/src/GitFileManager.cpp
+++ GitDive_Cinder/src/GitFileManager.cpp
@@ -72,9 +72,10 @@ void GitFileManager::applyDiffToFile(boost::shared_ptr<GitFile> file, boost::sha
     //Iterate over the original lines, removing or adding lines that match in the diff
     for(int lineNum = 1; lineNum <= originalLines.size() + diff->getNumDeltaLines(); lineNum++){
         
+        Line oldLine;
         try {
             cout << "--Line position:" << linePos << " Original size:" << originalLines.size() << " With delta:" << originalLines.size() + diff->getNumDeltaLines() << endl;
-            Line oldLine = originalLines[linePos-1];
+             = originalLines[linePos-1];
         } catch(std::exception e){
             cout << e.what();
         }
