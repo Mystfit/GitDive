2346c842efbf531a89d42f833c3ffb6c13d2980f
Byron Mallett <byronated@gmail.com>
2013-04-02 13:22:14 +1300

    Gitdive_autolog | MOOD:"mellow" | COFFEE:lipton iced tea | LOCATION:"masters couch"

--- GitDive_Cinder/src/GitFileManager.cpp
+++ GitDive_Cinder/src/GitFileManager.cpp
@@ -46,6 +46,10 @@ void GitFileManager::applyDiffToFile(boost::shared_ptr<GitFile> file, boost::sha
             {
                 Line cLine = diffHunk->getLine(j);
                 
+                
+                if(cLine.getLinePos())
+                
+                
                 if(strcmp(cLine.getStr().c_str(), oldLine.c_str()) && cLine.getLineState() == Line::LINE_DELETED){
                     linePos++;
                 } else if() {
