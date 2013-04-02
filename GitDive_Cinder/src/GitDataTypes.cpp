d02a27a50277cc4b3b7b6754a3af38a017607cf5
Byron Mallett <byronated@gmail.com>
2013-04-03 03:32:24 +1300

    Gitdive_autolog | MOOD:"frustrated" | COFFEE:none | LOCATION:"masters desk"

--- GitDive_Cinder/src/GitDataTypes.cpp
+++ GitDive_Cinder/src/GitDataTypes.cpp
@@ -126,6 +126,6 @@ GitFile::GitFile(string filename){
 
 void GitFile::resetLineOrder(){
     for(int i = 1; i <= m_activeLines.size(); i++){
-        m_activeLines[i].setLinePos(i);
+        m_activeLines[i-1].setLinePos(i);
     }
 }
