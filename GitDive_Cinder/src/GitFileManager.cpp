b038b6e4aab2cd4ba82205c4f342c320f1280f49
Byron Mallett <byronated@gmail.com>
2013-04-02 13:39:36 +1300

    Gitdive_autolog | MOOD:"mellow" | COFFEE:chocolate | LOCATION:"masters couch"

--- GitDive_Cinder/src/GitFileManager.cpp
+++ GitDive_Cinder/src/GitFileManager.cpp
@@ -40,6 +40,8 @@ void GitFileManager::applyDiffToFile(boost::shared_ptr<GitFile> file, boost::sha
         string oldLine = originalLines[lineNum].getStr();
         vector<Line> deltaLines = diff->getAllDeltaLines();
         
+        
+        //Always increment the old line position so we get the correct original line number
         linePos++;
         
         
