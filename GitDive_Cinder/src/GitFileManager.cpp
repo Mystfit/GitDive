f0953a29444147495b531e8596666b0cb491a394
Byron Mallett <byronated@gmail.com>
2013-04-02 21:53:51 +1300

    Gitdive_autolog | MOOD:"frustrated" | COFFEE:none | LOCATION:"masters desk"

--- GitDive_Cinder/src/GitFileManager.cpp
+++ GitDive_Cinder/src/GitFileManager.cpp
@@ -86,7 +86,7 @@ void GitFileManager::applyDiffToFile(boost::shared_ptr<GitFile> file, boost::sha
             
             if(deltaIndex < deltaRemoveLines.size() && deltaRemoveLines[deltaIndex].getLinePos() == linePos+1){
             {
-                cout << endl << "Line " << deltaRemoveLines[deltaIndex].getLinePos() << endl;
+               
             } else {
                 origLine.setLinePos(lineNum);
                 interimLines.push_back(origLine);
