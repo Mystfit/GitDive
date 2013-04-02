8b08bfe1828f1add696d457f53745ac23f40dca0
Byron Mallett <byronated@gmail.com>
2013-04-02 18:20:53 +1300

    Gitdive_autolog | MOOD:"mellow" | COFFEE:none | LOCATION:"masters couch"

--- GitDive_Cinder/src/GitFileManager.cpp
+++ GitDive_Cinder/src/GitFileManager.cpp
@@ -79,7 +79,7 @@ void GitFileManager::applyDiffToFile(boost::shared_ptr<GitFile> file, boost::sha
         Line oldLine("empty");
         
         try {
-            cout << "--Line position:" << linePos << " Original size:" << originalLines.size() << " Delta:" << diff->getNumDeltaLines() <<  " New total:" << originalLines.size() + diff->getNumDeltaLines() << " " << originalLines[linePos-1].getStr() << endl;
+            cout << "--Line position:" << linePos << " Original size:" << originalLines.size() << " Delta:" << diff->getNumDeltaLines() <<  " New total:" << originalLines.size() + diff->getNumDeltaLines() << " Status:" << originalLines[linePos-1].getLineState() << endl;
             if(originalLines[linePos-1].getLineState() == Line::LINE_ADDED) linesAdded++;
             if(originalLines[linePos-1].getLineState() == Line::LINE_DELETED) linesRemoved++;
 
