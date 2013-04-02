c6f4759f9a32ab85e18b48c4d26ebef45e531add
Byron Mallett <byronated@gmail.com>
2013-04-02 13:15:43 +1300

    Gitdive_autolog | MOOD:"mellow" | COFFEE:lipton iced tea | LOCATION:"masters couch"

--- GitDive_Cinder/src/GitFileManager.cpp
+++ GitDive_Cinder/src/GitFileManager.cpp
@@ -35,7 +35,6 @@ void GitFileManager::applyDiffToFile(boost::shared_ptr<GitFile> file, boost::sha
     //Iterate over the original lines, removing or adding lines that match in the diff
     for(int lineNum = 0; lineNum < originalLines.size() + diff->getDeltaNumLines(); lineNum++){
         
-        string oldLine = originalLines[lineNum];
         
         for(int i = 0; i < diff->getNumHunks(); i++){
             shared_ptr<DiffHunk> diffHunk = diff->getDiffHunk(i);
