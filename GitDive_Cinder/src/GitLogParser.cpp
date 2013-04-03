4624fd86b6d22dfd5c8fbcaa95a546330dd26c28
Byron Mallett <byronated@gmail.com>
2013-04-03 22:54:12 +1300

    Gitdive_autolog | MOOD:"mellow" | COFFEE:none | LOCATION:"masters desk"

--- GitDive_Cinder/src/GitLogParser.cpp
+++ GitDive_Cinder/src/GitLogParser.cpp
@@ -10,7 +10,7 @@
 
 vector<Commit> GitLogParser::parseLog(string logString)
 {
-    shared_ptr< vector<Commit> > commitList(new vector<Commit>);
+    vector<Commit> commitList(new vector<Commit>);
     vector <string> diffBlock;
     
     //Split string into lines
