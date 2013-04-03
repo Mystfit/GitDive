da3781b8465730bb4d351764f50777061bf4b643
Byron Mallett <byronated@gmail.com>
2013-04-03 22:44:00 +1300

    Gitdive_autolog | MOOD:"mellow" | COFFEE:none | LOCATION:"masters desk"

--- GitDive_Cinder/src/GitCommitManager.h
+++ GitDive_Cinder/src/GitCommitManager.h
@@ -9,6 +9,17 @@
 #ifndef __GitDive_Cinder__GitCommitManager__
 #define __GitDive_Cinder__GitCommitManager__
 
-#include <iostream>
+#include "GitDataTypes.h"
+
+
+class GitCommitManager {
+public:
+    //Commit traversal
+    boost::shared_ptr< vector<Commit> > getCommitSource(){ return m_commitList; };
+    void setCommitSource(boost::shared_ptr< vector<Commit> > commitList){ m_commitList = commitList; };
+    bool applyNextCommit();
+    int getCommitIndex(){ return m_commitIndex; };
+private:
+};
 
 #endif /* defined(__GitDive_Cinder__GitCommitManager__) */
