0af40f744acf4860a414e533969199afa9abc52d
Byron Mallett <byronated@gmail.com>
2013-04-03 13:30:18 +1300

    Gitdive_autolog | MOOD:"frustrated" | COFFEE:none | LOCATION:"masters desk"

--- GitDive_NoCinder/GitDive_NoCinder/GitDive_NoCinder/main.cpp
+++ GitDive_NoCinder/GitDive_NoCinder/GitDive_NoCinder/main.cpp
@@ -16,9 +16,41 @@
 
 int main(int argc, const char * argv[])
 {
+    string repoPath = "/Users/mystfit/Code/Python/Gitdive_Prototypes";
+    string gitCmd = "git log -p --reverse --pretty=format:\"GD_commit&%H^GD_commitAuthor&%cn^GD_date&%cd^GD_message&%B\"";
+    string combinedCmd = "cd " + repoPath + " && " + gitCmd;
+    
+    //Get the output of the git command from stdout as a giant string
+    string cmdOutput = Utils::getCmdOutput(combinedCmd.c_str());
+    
+    //Split string into lines
+    vector<string> outputStrings;
+    boost::split(outputStrings, cmdOutput, boost::is_any_of("\n"));
+    
+    //Send lines through the log parser to make commit objects
+    commitList = GitLogParser::parseLog(outputStrings);
+    
+    for(int commitIndex = 0; commitIndex < commitList.size(); commitIndex++){
+        fManager.updateFilesFromCommit(commitList[commitIndex]);
+    }
+    
+    //Test of the commit serializer
+    ofstream diffOutput;
+    ofstream fileOutput;
+    
+    diffOutput.open("/Users/mystfit/desktop/cinderDiffOut.log");
+    fileOutput.open("/Users/mystfit/desktop/cinderFileOut.log");
+    
+    for(int i = 0; i < commitList.size(); i++) {
+        diffOutput << GitLogParser::serializeCommit(commitList[i]);
+    }
+    
+    fileOutput << fManager.serializeAllFiles();
+    
+    //Close the file
+    diffOutput.close();
+    fileOutput.close();
 
-    // insert code here...
-    std::cout << "Hello, World!\n";
     return 0;
 }
 
