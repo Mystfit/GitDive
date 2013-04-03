f9f17a86334c8cd7ca58a6d575b2748da99ce81f
Byron Mallett <byronated@gmail.com>
2013-04-03 22:43:09 +1300

    Gitdive_autolog | MOOD:"mellow" | COFFEE:none | LOCATION:"masters desk"

---
+++ GitDive_Cinder/src/GitCommitManager.cpp
@@ -0,0 +1,9 @@
+//
+//  GitCommitManager.cpp
+//  GitDive_Cinder
+//
+//  Created by Byron Mallett on 3/04/13.
+//
+//
+
+#include "GitCommitManager.h"
---
+++ GitDive_Cinder/src/GitCommitManager.h
@@ -0,0 +1,14 @@
+//
+//  GitCommitManager.h
+//  GitDive_Cinder
+//
+//  Created by Byron Mallett on 3/04/13.
+//
+//
+
+#ifndef __GitDive_Cinder__GitCommitManager__
+#define __GitDive_Cinder__GitCommitManager__
+
+#include <iostream>
+
+#endif /* defined(__GitDive_Cinder__GitCommitManager__) */
