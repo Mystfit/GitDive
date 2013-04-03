a2d14b05db7fef6260bc7e7a7f2319587e129084
Byron Mallett <byronated@gmail.com>
2013-04-03 13:31:12 +1300

    Gitdive_autolog | MOOD:"frustrated" | COFFEE:none | LOCATION:"masters desk"

--- GitDive_NoCinder/GitDive_NoCinder/GitDive_NoCinder/main.cpp
+++ GitDive_NoCinder/GitDive_NoCinder/GitDive_NoCinder/main.cpp
@@ -16,6 +16,11 @@
 
 int main(int argc, const char * argv[])
 {
+    string repoPath;
+    string gitCmd;
+    GitFileManager fManager;
+    vector<Commit> commitList;
+    
     string repoPath = "/Users/mystfit/Code/Python/Gitdive_Prototypes";
     string gitCmd = "git log -p --reverse --pretty=format:\"GD_commit&%H^GD_commitAuthor&%cn^GD_date&%cd^GD_message&%B\"";
     string combinedCmd = "cd " + repoPath + " && " + gitCmd;
