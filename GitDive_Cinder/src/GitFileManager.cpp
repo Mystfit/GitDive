c9c680729eeb80b31bfde2f5365f0c340248ef16
Byron Mallett <byronated@gmail.com>
2013-04-04 13:52:41 +1300

    Gitdive_autolog | MOOD:"chirpy" | COFFEE:V iced coffee | LOCATION:"masters desk"

--- GitDive_Cinder/src/GitFileManager.cpp
+++ GitDive_Cinder/src/GitFileManager.cpp
@@ -113,7 +113,6 @@ void GitFileManager::applyDiffToFile(GitFile &file, boost::shared_ptr<Diff> diff
                     }
                 }
                 
-                    
             }
             deltaIndex++;
         }
@@ -123,6 +122,7 @@ void GitFileManager::applyDiffToFile(GitFile &file, boost::shared_ptr<Diff> diff
     //Reset counters
     deltaIndex = 0;
     int lineCounter = 0;
+    blockOpen = false;
     
     //Add lines
     if(deltaAddLines.size() == 0){
@@ -140,7 +140,6 @@ void GitFileManager::applyDiffToFile(GitFile &file, boost::shared_ptr<Diff> diff
                     block.blockStart = deltaAddLines[deltaIndex].getLinePos();
                     block.blockEnd = deltaAddLines[deltaIndex].getLinePos();
                 } else {
-                    
                     block.blockEnd = deltaAddLines[deltaIndex].getLinePos();
                 }
 
