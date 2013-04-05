b53967343d7f644a7a290bf700e0fac7466c46e7
Byron Mallett <byronated@gmail.com>
2013-04-06 00:55:35 +1300

    Gitdive_autolog | MOOD:"satisfied" | COFFEE:none | LOCATION:"masters desk"

--- GitDive_Cinder/src/GitFileManager.h
+++ GitDive_Cinder/src/GitFileManager.h
@@ -41,9 +41,14 @@ public:
     void saveFileToDisk(boost::shared_ptr<GitFile> file, string path);
     string serializeAllFiles();
     string serializeFile(boost::shared_ptr<GitFile> file);
+    
+    //Syntax highlighting
+    
 
     
 private:
+    SourceHighlight srcHiglight;
+
     vector<Commit> m_commitList;
     vector< boost::shared_ptr<GitFile> > m_fileList;
     
