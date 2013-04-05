10f654abf518231d35992b7c4791efd86d540e53
Byron Mallett <byronated@gmail.com>
2013-04-06 04:25:11 +1300

    Gitdive_autolog | MOOD:"satisfied" | COFFEE:none | LOCATION:"masters desk"

--- GitDive_Cinder/src/GitFileManager.cpp
+++ GitDive_Cinder/src/GitFileManager.cpp
@@ -296,7 +296,7 @@ string GitFileManager::colourfyFile(GitFile & file){
     
     string inputLang = "cpp.lang";
     srchilite::LangMap langMap("", "lang.map");
-    string lang = langMap.getMappedFileNameFromFileName(argv[1]);
+    string lang = langMap.getMappedFileNameFromFileName(file.getFilename());
     if (lang != "") {
         inputLang = lang;
     } // otherwise we default to C++
