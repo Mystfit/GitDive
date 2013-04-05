7638f3fe3192cb949ea899d0fc90ebbb137d7fdb
Byron Mallett <byronated@gmail.com>
2013-04-06 01:07:53 +1300

    Gitdive_autolog | MOOD:"satisfied" | COFFEE:none | LOCATION:"masters desk"

--- GitDive_Cinder/src/GitDataTypes.cpp
+++ GitDive_Cinder/src/GitDataTypes.cpp
@@ -148,8 +148,9 @@ string GitFile::getAsColourifiedStr(){
 
 stringstream GitFile::getAsStringStream(){
     stringstream outStream;
+    
     for(vector< boost::shared_ptr<Line> >::iterator it = m_activeLines.begin(); it != m_activeLines.end(); ++it) {
-        
+        outStream << it->getStr();
     }
     
     
