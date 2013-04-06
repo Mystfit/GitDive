b09490dd0f29e40cc3b8e499bb8ec862c547b016
Byron Mallett <byronated@gmail.com>
2013-04-07 01:52:00 +1300

    Gitdive_autolog | MOOD:"satisfied" | COFFEE:none | LOCATION:"masters desk"

--- GitDive_Cinder/src/SyntaxColourTools.h
+++ GitDive_Cinder/src/SyntaxColourTools.h
@@ -62,7 +62,7 @@ private:
 class LineFormatter: public srchilite::Formatter
 {    
 public:
-    LineFormatter(const std::string &elem_ = "normal", boost::shared_ptr<Line> line) : elem(elem_), m_targetLine(line) {}
+    LineFormatter(const std::string &elem_, boost::shared_ptr<Line> line) : elem(elem_), m_targetLine(line) {}
     void setTargetLine(boost::shared_ptr<Line> line){m_targetLine = line; };
     void format(const std::string &s, const srchilite::FormatterParams *params = 0);
 private:
