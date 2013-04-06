6d1fc44b05030caa74ed3a75666c54084b2c77fb
Byron Mallett <byronated@gmail.com>
2013-04-07 00:22:39 +1300

    Gitdive_autolog | MOOD:"satisfied" | COFFEE:none | LOCATION:"masters desk"

--- GitDive_Cinder/src/SyntaxColourTools.h
+++ GitDive_Cinder/src/SyntaxColourTools.h
@@ -76,7 +76,7 @@ private:
 class LineHighlighter : public srchilite::SourceHighlighter {\
 public:
     virtual void format(const std::string &elem, const std::string &s);
-    void setElemFormatterManager(boost::shared_ptr<ElemFormatManager> manager){  };
+    void setElemFormatterManager(boost::shared_ptr<ElemFormatManager> manager){ formatterManager };
 private:
     boost::shared_ptr<ElemFormatManager> formatterManager;
 };
