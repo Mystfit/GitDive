7389125a39d6bafdb2eefc1682747158037866a5
Byron Mallett <byronated@gmail.com>
2013-04-07 02:02:32 +1300

    Gitdive_autolog | MOOD:"satisfied" | COFFEE:none | LOCATION:"masters desk"

--- GitDive_Cinder/src/GitLogParser.cpp
+++ GitDive_Cinder/src/GitLogParser.cpp
@@ -143,7 +143,7 @@ vector< boost::shared_ptr<Diff> > GitLogParser::parseCommit(vector<string> diffB
             if(!inDiffHeader){
                 
                 if(starts_with(line, "\\ No newline at end of file")) continue;
-                line.replace
+                line.Replace
                 Line cLine(line);
                     
                 //If the line is to be removed, stick with the old line position
