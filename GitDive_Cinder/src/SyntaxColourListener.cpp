af0cab04f32a5d6ac898b087726583a50e9aaf3b
Byron Mallett <byronated@gmail.com>
2013-04-06 19:32:32 +1300

    Gitdive_autolog | MOOD:"satisfied" | COFFEE:none | LOCATION:"masters desk"

---
+++ GitDive_Cinder/src/SyntaxColourListener.cpp
@@ -0,0 +1,9 @@
+//
+//  SyntaxColourListener.cpp
+//  GitDive_Cinder
+//
+//  Created by Byron Mallett on 6/04/13.
+//
+//
+
+#include "SyntaxColourListener.h"
---
+++ GitDive_Cinder/src/SyntaxColourListener.h
@@ -0,0 +1,14 @@
+//
+//  SyntaxColourListener.h
+//  GitDive_Cinder
+//
+//  Created by Byron Mallett on 6/04/13.
+//
+//
+
+#ifndef __GitDive_Cinder__SyntaxColourListener__
+#define __GitDive_Cinder__SyntaxColourListener__
+
+#include <iostream>
+
+#endif /* defined(__GitDive_Cinder__SyntaxColourListener__) */
