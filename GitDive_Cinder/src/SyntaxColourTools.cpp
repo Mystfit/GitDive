a2873834671108d49349126c5cacc0423efbca10
Byron Mallett <byronated@gmail.com>
2013-04-07 02:07:12 +1300

    Gitdive_autolog | MOOD:"satisfied" | COFFEE:none | LOCATION:"masters desk"

--- GitDive_Cinder/src/SyntaxColourTools.cpp
+++ GitDive_Cinder/src/SyntaxColourTools.cpp
@@ -9,16 +9,6 @@
 #include "SyntaxColourTools.h"
 
 
-//LineHighlighter::LineHighlighter(HighlightStatePtr mainState) : mainHighlightState(mainState), currentHighlightState(mainState)
-//{
-//    
-//}
-//void LineHighLighter::format(const std::string &elem, const std::string &s) {
-//    formatterManager->getFormatter(elem);
-//    SourceHighlighter::format(elem, s);
-//}
-
-
 SyntaxColourListener::SyntaxColourListener(){
 }
 
@@ -26,40 +16,40 @@ SyntaxColourListener::~SyntaxColourListener(){
 }
 
 void SyntaxColourListener::notify(const srchilite::HighlightEvent &event){
-//    cout << event.type << " on line " << m_targetLine->getStr() << endl;
-//    switch (event.type) {
-//        case HighlightEvent::FORMAT:
-//            // print information about the rule
-//            if (event.token.rule) {
-//                cout << event.token.rule->getAdditionalInfo() << endl;
-//                //cout << "expression: \"" << event.token.rule->toString() << "\""<< endl;
-//            }
-//            
-//            // now format the matched strings
-//            for (MatchedElements::const_iterator it = event.token.matched.begin(); it
-//                 != event.token.matched.end(); ++it) {
-//                cout << "formatting \"" << it->second << "\" as " << it->first
-//                << endl;
-//            }
-//            break;
-//        case HighlightEvent::FORMATDEFAULT:
-//            cout << "formatting \"" << event.token.matched.front().second
-//            << "\" as default" << endl;
-//            break;
-//        case HighlightEvent::ENTERSTATE:
-//            cout << "entering state: " << event.token.rule->getNextState()->getId()
-//            << endl;
-//            break;
-//        case HighlightEvent::EXITSTATE:
-//            int level = event.token.rule->getExitLevel();
-//            cout << "exiting state, level: ";
-//            if (level < 0)
-//                cout << "all";
-//            else
-//                cout << level;
-//            cout << endl;
-//            break;
-//    }
+    cout << event.type << " on line " << m_targetLine->getStr() << endl;
+    switch (event.type) {
+        case HighlightEvent::FORMAT:
+            // print information about the rule
+            if (event.token.rule) {
+                cout << event.token.rule->getAdditionalInfo() << endl;
+                //cout << "expression: \"" << event.token.rule->toString() << "\""<< endl;
+            }
+            
+            // now format the matched strings
+            for (MatchedElements::const_iterator it = event.token.matched.begin(); it
+                 != event.token.matched.end(); ++it) {
+                cout << "formatting \"" << it->second << "\" as " << it->first
+                << endl;
+            }
+            break;
+        case HighlightEvent::FORMATDEFAULT:
+            cout << "formatting \"" << event.token.matched.front().second
+            << "\" as default" << endl;
+            break;
+        case HighlightEvent::ENTERSTATE:
+            cout << "entering state: " << event.token.rule->getNextState()->getId()
+            << endl;
+            break;
+        case HighlightEvent::EXITSTATE:
+            int level = event.token.rule->getExitLevel();
+            cout << "exiting state, level: ";
+            if (level < 0)
+                cout << "all";
+            else
+                cout << level;
+            cout << endl;
+            break;
+    }
 }
 
 
