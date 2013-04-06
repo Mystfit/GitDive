21b9f624acc7469b0a9bef948d2fa8c6d53845d7
Byron Mallett <byronated@gmail.com>
2013-04-06 19:48:26 +1300

    Gitdive_autolog | MOOD:"satisfied" | COFFEE:none | LOCATION:"masters desk"

--- GitDive_Cinder/src/GitFileManager.cpp
+++ GitDive_Cinder/src/GitFileManager.cpp
@@ -319,6 +319,7 @@ void GitFileManager::syntaxParseFile(GitFile &file ){
     vector< boost::shared_ptr<Line> > lines = file.getLines();
     for(int i = 0; i < lines.size(); i++){
         params.start = 0;
+        colourListener->setTargetLine(lines[i]);
         
         highlighter.highlightParagraph(lines[i]->getStr());
     }
