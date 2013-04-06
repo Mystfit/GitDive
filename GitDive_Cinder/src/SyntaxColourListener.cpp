8fb0cac39cdb5d12f49a6b37339462bfc6e30004
Byron Mallett <byronated@gmail.com>
2013-04-06 21:37:25 +1300

    Gitdive_autolog | MOOD:"satisfied" | COFFEE:none | LOCATION:"masters desk"

--- GitDive_Cinder/src/SyntaxColourListener.cpp
+++ GitDive_Cinder/src/SyntaxColourListener.cpp
@@ -16,4 +16,41 @@ SyntaxColourListener::~SyntaxColourListener(){
 
 void SyntaxColourListener::notify(const srchilite::HighlightEvent &event){
     cout << event.type << " on line " << m_targetLine->getStr() << endl;
+    
+    switch (event.type) {
+        case HighlightEvent::FORMAT:
+            // print information about the rule
+            if (event.token.rule) {
+                cout << event.token.rule->getAdditionalInfo() << endl;
+                cout << "expression: \"" << event.token.rule->toString() << "\""
+                << endl;
+            }
+            
+            // now format the matched strings
+            for (MatchedElements::const_iterator it = event.token.matched.begin(); it
+                 != event.token.matched.end(); ++it) {
+                cout << "formatting \"" << it->second << "\" as " << it->first
+                << endl;
+            }
+            step();
+            break;
+        case HighlightEvent::FORMATDEFAULT:
+            cout << "formatting \"" << event.token.matched.front().second
+            << "\" as default" << endl;
+            step();
+            break;
+        case HighlightEvent::ENTERSTATE:
+            cout << "entering state: " << event.token.rule->getNextState()->getId()
+            << endl;
+            break;
+        case HighlightEvent::EXITSTATE:
+            int level = event.token.rule->getExitLevel();
+            cout << "exiting state, level: ";
+            if (level < 0)
+                cout << "all";
+            else
+                cout << level;
+            cout << endl;
+            break;
+    }
 }
