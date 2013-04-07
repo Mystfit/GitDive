249aa3bfcfb3578df08466004cf5115a011adeed
Byron Mallett <byronated@gmail.com>
2013-04-08 02:04:29 +1200

    Gitdive_autolog | MOOD:"satisfied" | COFFEE:none | LOCATION:"train"

--- GitDive_Cinder/src/GitDive_CinderApp.cpp
+++ GitDive_Cinder/src/GitDive_CinderApp.cpp
@@ -20,8 +20,7 @@ void GitDive_CinderApp::setup()
     fManager.setSyntaxHighlightStatus(true);
     
     mParams = params::InterfaceGl( "App parameters", Vec2i( 200, 400 ) );
-    mParams.addParam( "Use flat shading", &fManager.getSyntaxHighlightStatus());
-
+    mParams.addParam( "Split lines by syntax", &fManager.getSyntaxHighlightStatus());
 
     
 //    while(fManager.applyNextCommit());
