1a794390fb751374826750786289ce3e32f174de
Byron Mallett <byronated@gmail.com>
2013-04-08 00:28:49 +1200

    Gitdive_autolog | MOOD:"satisfied" | COFFEE:none | LOCATION:"train"

--- GitDive_Cinder/src/GitDataTypes.cpp
+++ GitDive_Cinder/src/GitDataTypes.cpp
@@ -37,7 +37,7 @@ string Line::getStr(){
         lineStr << m_lineStr;
     } else {
         //cout << m_lineElements.size() << "|";
-        for(vector<LineElement>::iterator it = m_lineElements.begin(); it != m_lineElements.end()-1; ++it){
+        for(vector<LineElement>::iterator it = m_lineElements.begin(); it != m_lineElements.end(); ++it){
             //cout << it->str << "|";
             //if(it->str != "\n")
             lineStr << it->str;
