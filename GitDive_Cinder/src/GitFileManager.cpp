defcdcb0e6fdcd35977702efafb29ae9514ed966
Byron Mallett <byronated@gmail.com>
2013-04-07 14:47:04 +1200

    Gitdive_autolog | MOOD:"satisfied" | COFFEE:none | LOCATION:"home"

--- GitDive_Cinder/src/GitFileManager.cpp
+++ GitDive_Cinder/src/GitFileManager.cpp
@@ -282,7 +282,7 @@ string GitFileManager::serializeAllFiles(){
 string GitFileManager::serializeFile(boost::shared_ptr<GitFile> file){
     stringstream fileText;
     fileText << "<p><h3>File: " << file->getFilename() << "</h3></p>" << endl;
-    fileText << colourfyFile(*(file)); //file->getStr();
+    fileText << file->getStr(); //colourfyFile(*(file)); 
     fileText << "<p />" << endl << endl;
 
     return fileText.str();
