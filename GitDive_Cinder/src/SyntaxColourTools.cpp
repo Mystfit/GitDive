df759901ea7f3692cd1c88b5061f3127eef58af8
Byron Mallett <byronated@gmail.com>
2013-04-07 22:58:16 +1200

    Gitdive_autolog | MOOD:"satisfied" | COFFEE:none | LOCATION:"train"

--- GitDive_Cinder/src/SyntaxColourTools.cpp
+++ GitDive_Cinder/src/SyntaxColourTools.cpp
@@ -83,7 +83,9 @@ void LineFormatter::format(const std::string &s, const srchilite::FormatterParam
     
     if(elem == "") elem = "normal";
     
-    Utils::strip(s, "\n");
+    string str = s;
+    
+    Utils::strip(str, "\n");
 
     
     m_targetLine->addLineElement(LineElement(elem, s, params->start));
