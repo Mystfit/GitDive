6358d4bb91ab237aadd7d288805fc5db0bf41505
Byron Mallett <byronated@gmail.com>
2013-04-08 18:30:33 +1200

    Gitdive_autolog | MOOD:"excited" | COFFEE:none | LOCATION:"masters desk"

---
+++ GitDive_Cinder/src/TextRenderer.cpp
@@ -0,0 +1,9 @@
+//
+//  TextRenderer.cpp
+//  GitDive_Cinder
+//
+//  Created by Byron Mallett on 8/04/13.
+//
+//
+
+#include "TextRenderer.h"
---
+++ GitDive_Cinder/src/TextRenderer.h
@@ -0,0 +1,14 @@
+//
+//  TextRenderer.h
+//  GitDive_Cinder
+//
+//  Created by Byron Mallett on 8/04/13.
+//
+//
+
+#ifndef __GitDive_Cinder__TextRenderer__
+#define __GitDive_Cinder__TextRenderer__
+
+#include <iostream>
+
+#endif /* defined(__GitDive_Cinder__TextRenderer__) */
