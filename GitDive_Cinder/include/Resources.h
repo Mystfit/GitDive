ef303825a5dd04e5c612fbc356c80a72f233712b
Byron Mallett <byronated@gmail.com>
2013-04-08 19:59:16 +1200

    Gitdive_autolog | MOOD:"tired" | COFFEE:none | LOCATION:"masters desk"

--- GitDive_Cinder/include/Resources.h
+++ GitDive_Cinder/include/Resources.h
@@ -2,3 +2,5 @@
 #include "cinder/CinderResources.h"
 
 //#define RES_MY_RES			CINDER_RESOURCE( ../resources/, image_name.png, 128, IMAGE )
+#define RES_CUSTOM_FONT		CINDER_RESOURCE( ../resources/, Alfphabet-IV.ttf, 128, TTF )
+
