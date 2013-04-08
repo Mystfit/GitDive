6d62d2275e5f7d0761906cf711d6f667cd16cad2
Byron Mallett <byronated@gmail.com>
2013-04-08 22:39:31 +1200

    Gitdive_autolog | MOOD:"tired" | COFFEE:none | LOCATION:"masters desk"

--- GitDive_Cinder/src/TextAnimateable.cpp
+++ GitDive_Cinder/src/TextAnimateable.cpp
@@ -21,21 +21,20 @@ void TextAnimateable::draw(cinder::gl::TextureFont & texFont){
 
 
 void TextAnimateable::animIn( cinder::Timeline &timeline, cinder::Vec2<float> destVec, cinder::Color destColour )
-{    
+{
+    cinder::Tween<cinder::Vec2f> test = timeline.apply( &m_colour, destColour, 1.0f, easer);
+    
     cinder::EaseInAtan easer(20);
     timeline.apply( &m_colour, destColour, 1.0f, easer);
-    m_posTween = timeline.apply( &m_position, destVec, 0.5f);
+    timeline.apply( &m_position, destVec, 0.5f);
     //timeline.apply( &m_position, destVec, 0.5f).finishFn( std::bind( &TextAnimateable::onAnimIn, this ) );
 }
 
 void TextAnimateable::animOut( cinder::Timeline &timeline, cinder::Vec2<float> destVec, cinder::Color destColour )
 {
-    
-    cinder::Tween<cinder::Vec2f> test = timeline.apply(&m_position, destVec, 0.5f);
-    
-    cinder::EaseOutAtan easer(30);
+    cinder::EaseOutAtan easer(20);
     timeline.apply( &m_colour, destColour, 1.0f,  easer);
-    m_posTween = timeline.apply( &m_position, destVec, 0.5f, easer );
+    //timeline.apply( &m_position, destVec, 0.5f, cinder::easeOutAtan( 10 ) ).finishFn( std::bind( &TextAnimateable::onAnimIn, this ) );
 }
 
 std::string TextAnimateable::getStr()
