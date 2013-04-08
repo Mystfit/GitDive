240acf2ca627f2e7e2324e700bb6677c8ee567b3
Byron Mallett <byronated@gmail.com>
2013-04-08 20:45:37 +1200

    Gitdive_autolog | MOOD:"tired" | COFFEE:none | LOCATION:"masters desk"

--- GitDive_Cinder/src/TextAnimateable.h
+++ GitDive_Cinder/src/TextAnimateable.h
@@ -19,10 +19,10 @@ using namespace cinder;
 class TextAnimateable {
 public:
     TextAnimateable();
-    virtual void animIn( Timeline &timeline, Vec2<float> destVec, Color destColour )
+    virtual void animIn( Timeline &timeline, Vec2<float> position, Color colour )
     {
-        Anim<Vec2f> dest = destVec;
-        Anim<
+        Anim<Vec2f> destVec = position;
+        Anim<Color> destColour = colour;
         timeline.apply( &m_colour, destColour, 1.0f, EaseOutAtan( 20 ) );
         timeline.apply( &m_position, destVec, 0.5f, EaseOutAtan( 10 ) ).finishFn( std::bind( &TextAnimateable::onAnimIn, this ) );
     }
