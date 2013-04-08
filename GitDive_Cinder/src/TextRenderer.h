32bc11710736a71614cbac03b5326dd1d3d1ca0d
Byron Mallett <byronated@gmail.com>
2013-04-08 18:35:02 +1200

    Gitdive_autolog | MOOD:"excited" | COFFEE:none | LOCATION:"masters desk"

--- GitDive_Cinder/src/TextRenderer.h
+++ GitDive_Cinder/src/TextRenderer.h
@@ -16,10 +16,19 @@ class TextRenderer {
 public:
     TextRenderer();
     
+    //Starts tracking a line for rendering
     void trackLine(boost::shared_ptr<Line> line);
     
+    //Breaks line into component elements
+    void breakLine(boost::shared_ptr<Line> line);
+    
+    //Adds a new free line element to the pool
     void addFreeLineElem(LineElement lnElem);
+    
+    //Returns a free line element
     void getFreeLineElem(LineElement searchTarget);
+    
+    void removeLineElement()
     
 private:
     vector<LineElement> m_freeLineElements;
