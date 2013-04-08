af7db8697bce938bb2bd5870d59033b2d67d0a34
Byron Mallett <byronated@gmail.com>
2013-04-08 16:59:33 +1200

    Gitdive_autolog | MOOD:"excited" | COFFEE:none | LOCATION:"masters desk"

--- GitDive_Cinder/src/GitDive_CinderApp.cpp
+++ GitDive_Cinder/src/GitDive_CinderApp.cpp
@@ -35,7 +35,7 @@ void GitDive_CinderApp::setup()
 
 void GitDive_CinderApp::initUI(){
     //Setup UI
-    mParams = params::InterfaceGl( "App parameters", Vec2i( 200, 400 ) );
+    mParams = params::InterfaceGl( "App parameters", Vec2i( 400, 400 ) );
     mParams.addParam("Repo path", &m_repoPath);
     mParams.addParam("Split lines by syntax", &fManager.getSyntaxHighlightStatus());
     mParams.addParam("Output syntax to file", &bOutputSyntax);
