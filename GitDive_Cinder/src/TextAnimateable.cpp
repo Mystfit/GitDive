d788fd3ac839d0d69addda8447d59bd1bdb89b97
Byron Mallett <byronated@gmail.com>
2013-04-08 22:43:28 +1200

    Gitdive_autolog | MOOD:"tired" | COFFEE:none | LOCATION:"masters desk"

--- GitDive_Cinder/src/TextAnimateable.cpp
+++ GitDive_Cinder/src/TextAnimateable.cpp
@@ -34,8 +34,10 @@ void TextAnimateable::animIn( cinder::Timeline &timeline, cinder::Vec2<float> de
 void TextAnimateable::animOut( cinder::Timeline &timeline, cinder::Vec2<float> destVec, cinder::Color destColour )
 {
     cinder::EaseOutAtan easer(20);
+    cinder::EaseInAtan easer(20);
     m_colourTween = timeline.apply( &m_colour, destColour, 1.0f,  easer);
     m_posTween = timeline.apply( &m_position, destVec, 0.5f,  easer);
+    //timeline.apply( &m_colour, destColour, 1.0f,  easer);
     //timeline.apply( &m_position, destVec, 0.5f, cinder::easeOutAtan( 10 ) ).finishFn( std::bind( &TextAnimateable::onAnimIn, this ) );
 }
 
