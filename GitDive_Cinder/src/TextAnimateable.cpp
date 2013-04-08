8eab2984e3aaa935ec4c041289797728f621c021
Byron Mallett <byronated@gmail.com>
2013-04-08 22:18:03 +1200

    Gitdive_autolog | MOOD:"tired" | COFFEE:none | LOCATION:"masters desk"

--- GitDive_Cinder/src/TextAnimateable.cpp
+++ GitDive_Cinder/src/TextAnimateable.cpp
@@ -21,7 +21,7 @@ void TextAnimateable::draw(cinder::gl::TextureFont & texFont){
 
 void TextAnimateable::animIn( cinder::Timeline &timeline, cinder::Vec2<float> destVec, cinder::Color destColour )
 {
-    timeline.apply( &m_colour, destColour, 1.0f, cinder::easeOutAtan( 20 ) );
+    timeline.apply( &m_colour, destColour );
     //timeline.apply( &m_position, destVec, 0.5f, cinder::easeOutAtan( 10 ) ).finishFn( std::bind( &TextAnimateable::onAnimIn, this ) );
 }
 
