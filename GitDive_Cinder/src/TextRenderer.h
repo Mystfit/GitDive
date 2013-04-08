5425ff948e90ce2226e59cb4823d6903451ec93d
Byron Mallett <byronated@gmail.com>
2013-04-08 19:48:28 +1200

    Gitdive_autolog | MOOD:"tired" | COFFEE:none | LOCATION:"masters desk"

--- GitDive_Cinder/src/TextRenderer.h
+++ GitDive_Cinder/src/TextRenderer.h
@@ -36,7 +36,7 @@ private:
     vector<LineElement> m_freeLineElements;
     vector<LineElement> m_tweeingElements;
     
-	gl::TextureFontRef	mTextureFont;
+	cinder::gl::TextureFontRef	mTextureFont;
     
 };
 
