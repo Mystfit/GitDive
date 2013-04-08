189fe3dd7a12eb8471d296f134a8cffe55183d43
Byron Mallett <byronated@gmail.com>
2013-04-08 20:54:26 +1200

    Gitdive_autolog | MOOD:"tired" | COFFEE:none | LOCATION:"masters desk"

--- GitDive_Cinder/src/TextAnimateable.h
+++ GitDive_Cinder/src/TextAnimateable.h
@@ -19,7 +19,7 @@ using namespace cinder;
 
 class TextAnimateable {
 public:
-    TextAnimateable(gl::TextureFontRef & textureFont) : m_textureFont(textureFont)
+    TextAnimateable(gl::TextureFontRef & textureFont) : m_textureFont(textureFont);
     virtual void draw();
     
     virtual void animIn( Timeline &timeline, Vec2<float> destVec, Color destColour )
