8abdb00ce146356b90202c95868239a1f0289317
Byron Mallett <byronated@gmail.com>
2013-04-08 22:07:05 +1200

    Gitdive_autolog | MOOD:"tired" | COFFEE:none | LOCATION:"masters desk"

--- GitDive_Cinder/src/TextAnimateable.cpp
+++ GitDive_Cinder/src/TextAnimateable.cpp
@@ -7,9 +7,31 @@
 //
 
 #include "TextAnimateable.h"
+
+TextAnimateable::TextAnimateable(){
+    
+}
+
                 
 void TextAnimateable::draw(cinder::gl::TextureFont & texFont){
     cinder::gl::color( m_colour );
     texFont.drawString( getStr(), m_position);
 }
-            
+
+
+void TextAnimateable::animIn( cinder::Timeline &timeline, cinder::Vec2<float> destVec, cinder::Color destColour )
+{
+    //timeline.apply( &m_colour, destColour, 1.0f, cinder::easeOutAtan( 20 ) );
+    //timeline.apply( &m_position, destVec, 0.5f, cinder::easeOutAtan( 10 ) ).finishFn( std::bind( &TextAnimateable::onAnimIn, this ) );
+}
+
+void TextAnimateable::animOut( cinder::Timeline &timeline, cinder::Vec2<float> destVec, cinder::Color destColour )
+{
+    //timeline.apply( &m_colour, destColour, 1.0f, cinder::easeOutAtan( 20 ) );
+    //timeline.apply( &m_position, destVec, 0.5f, cinder::easeOutAtan( 10 ) ).finishFn( std::bind( &TextAnimateable::onAnimIn, this ) );
+}
+
+virtual std::string TextAnimateable::getStr()
+{
+    
+}
