c52364256884cad47433000fecc587c70cf7adde
Byron Mallett <byronated@gmail.com>
2013-04-08 20:10:11 +1200

    Gitdive_autolog | MOOD:"tired" | COFFEE:none | LOCATION:"masters desk"

--- GitDive_Cinder/src/TextRenderer.cpp
+++ GitDive_Cinder/src/TextRenderer.cpp
@@ -9,7 +9,7 @@
 #include "TextRenderer.h"
 
 TextRenderer::TextRenderer(){
-    cinder::Font customFont( Font( "Lucidia Console", 100 ) );
+    cinder::Font customFont( Font( "Lucida Console", 100 ) );
 	cinder::gl::TextureFont::Format f;
 	f.enableMipmapping( true );
 	m_textureFont = gl::TextureFont::create( customFont, f );
