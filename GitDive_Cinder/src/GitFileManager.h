1a4907efb118df0e7bc5f89402d26f3a99773f04
Byron Mallett <byronated@gmail.com>
2013-04-08 21:25:09 +1200

    Gitdive_autolog | MOOD:"tired" | COFFEE:none | LOCATION:"masters desk"

--- GitDive_Cinder/src/GitFileManager.h
+++ GitDive_Cinder/src/GitFileManager.h
@@ -28,7 +28,7 @@ using namespace std;
 class GitFileManager {
 public:
     GitFileManager();
-    void setTextRenderer( boost_shared_ptr<TextRenderer> tRender ){ m_tRender = tRender };
+    void setTextRenderer( boost::shared_ptr<TextRenderer> tRender ){ m_tRender = tRender };
     
     //File addition/retrieval
     //-----------------------
