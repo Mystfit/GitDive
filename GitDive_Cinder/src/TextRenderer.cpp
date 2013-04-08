6df02de7c8996448445ae68e24c7be9de9dd6749
Byron Mallett <byronated@gmail.com>
2013-04-09 00:18:14 +1200

    Gitdive_autolog | MOOD:"tired" | COFFEE:none | LOCATION:"masters desk"

--- GitDive_Cinder/src/TextRenderer.cpp
+++ GitDive_Cinder/src/TextRenderer.cpp
@@ -26,7 +26,7 @@ void TextRenderer::makeLineFromFreeElements(boost::shared_ptr<Line> line){
 void TextRenderer::animLinesIn(vector<boost::shared_ptr<Line> > lines){
     for(vector<boost::shared_ptr<Line> >::iterator it = lines.begin(); it != lines.end(); ++it){
         if(it->get()->getLineState() == Line::LINE_ADDED){
-            it->get()->animIn(cinde, Vec2f(0, it->get()->getLinePos() * 10, Color(255,255,255));
+            it->get()->animIn(m_timeline, Vec2f(0, it->get()->getLinePos() * 10, Color(255,255,255));
         }
     }
 }
