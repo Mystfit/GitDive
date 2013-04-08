a996ecd716eeec24c4187d43987c7d122bb349fa
Byron Mallett <byronated@gmail.com>
2013-04-08 18:02:56 +1200

    Gitdive_autolog | MOOD:"excited" | COFFEE:none | LOCATION:"masters desk"

--- GitDive_Cinder/src/GitDive_CinderApp.cpp
+++ GitDive_Cinder/src/GitDive_CinderApp.cpp
@@ -106,7 +106,6 @@ void GitDive_CinderApp::keyDown(KeyEvent event){
 void GitDive_CinderApp::update()
 {
     if(bVizActive){
-        cout << floor(ci::app::getElapsedFrames() % int(ci::app::getFrameRate()) / m_timeSpeed) << " " << bFframeReady << endl;
         int frameNum = floor(ci::app::getElapsedFrames() % int(ci::app::getFrameRate() / m_timeSpeed));
         if(frameNum == 0 && frameNum != m_lastFrame){
             bFframeReady = true;
