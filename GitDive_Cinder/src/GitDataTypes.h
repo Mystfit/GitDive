57bab47a3450bda77ee7fccd4471371609b7186b
Byron Mallett <byronated@gmail.com>
2013-04-08 20:48:52 +1200

    Gitdive_autolog | MOOD:"tired" | COFFEE:none | LOCATION:"masters desk"

--- GitDive_Cinder/src/GitDataTypes.h
+++ GitDive_Cinder/src/GitDataTypes.h
@@ -23,7 +23,7 @@ using namespace std;
  * A single line
  */
 
-struct LineElement {
+struct LineElement : public TextAnimateable {
     LineElement(string element, string elemStr, int pos) : elem(element), str(elemStr), position(pos)
     {
         if(elem == "comment"){
