7f7c4b7590f27bebae40f128d95d996b3196fced
Byron Mallett <byronated@gmail.com>
2013-04-08 21:25:51 +1200

    Gitdive_autolog | MOOD:"tired" | COFFEE:none | LOCATION:"masters desk"

--- GitDive_Cinder/src/GitFileManager.cpp
+++ GitDive_Cinder/src/GitFileManager.cpp
@@ -153,7 +153,7 @@ void GitFileManager::applyDiffToFile(GitFile &file, boost::shared_ptr<Diff> diff
             if(deltaIndex < deltaAddLines.size() && deltaAddLines[deltaIndex].getLinePos() == lineNum){
                 
                 //Create new line pointer
-                boost::shared_ptr<Line> newLine( new Line(&m_tRender.getTexFont(), deltaAddLines[deltaIndex].getStr(), true) );
+                boost::shared_ptr<Line> newLine( new Line(deltaAddLines[deltaIndex].getStr(), true) );
                 newLine->setLinePos(deltaAddLines[deltaIndex].getLinePos());
                 newLine->setLineState(Line::LINE_ADDED);
                 newLines.push_back(newLine);
