1b3eed1d224760c9a5f2325666fe795ebbb46456
Byron Mallett <byronated@gmail.com>
2013-04-08 20:33:12 +1200

    Gitdive_autolog | MOOD:"tired" | COFFEE:none | LOCATION:"masters desk"

---
+++ GitDive_Cinder/src/TextAnimateable.cpp
@@ -0,0 +1,9 @@
+//
+//  TextAnimateable.cpp
+//  GitDive_Cinder
+//
+//  Created by Byron Mallett on 8/04/13.
+//
+//
+
+#include "TextAnimateable.h"
---
+++ GitDive_Cinder/src/TextAnimateable.h
@@ -0,0 +1,14 @@
+//
+//  TextAnimateable.h
+//  GitDive_Cinder
+//
+//  Created by Byron Mallett on 8/04/13.
+//
+//
+
+#ifndef __GitDive_Cinder__TextAnimateable__
+#define __GitDive_Cinder__TextAnimateable__
+
+#include <iostream>
+
+#endif /* defined(__GitDive_Cinder__TextAnimateable__) */
