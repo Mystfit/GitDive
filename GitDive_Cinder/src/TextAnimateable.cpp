f29d0b6474f07003f01967d7c6324260a7ae769f
Byron Mallett <byronated@gmail.com>
2013-04-08 20:53:07 +1200

    Gitdive_autolog | MOOD:"tired" | COFFEE:none | LOCATION:"masters desk"

--- GitDive_Cinder/src/TextAnimateable.cpp
+++ GitDive_Cinder/src/TextAnimateable.cpp
@@ -7,3 +7,27 @@
 //
 
 #include "TextAnimateable.h"
+
+
+
+TextAnimateable::TextAnimateable(){
+    
+}
+                
+TextAnimateable::draw(){
+    gl::color( mColorCur );
+    gl::pushMatrices();
+    Matrix44f m = mMatrix;
+    m.scale( Vec3f( 1.0f, -1.0f, 1.0 ) );
+    gl::multModelView( m );
+    mTextureFont->drawString( mChar, mKernBounds.getCenter() - Vec2f( mKernBounds.getWidth(), 0.0f ) );
+    gl::popMatrices();
+}
+                
+gl::color( mColorCur );
+gl::pushMatrices();
+Matrix44f m = mMatrix;
+m.scale( Vec3f( 1.0f, -1.0f, 1.0 ) );
+gl::multModelView( m );
+mTextureFont->drawString( mChar, mKernBounds.getCenter() - Vec2f( mKernBounds.getWidth(), 0.0f ) );
+gl::popMatrices();
