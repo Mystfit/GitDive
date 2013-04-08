92d722e4063fc68edff3462b2576c56c6d2cd8dc
Byron Mallett <byronated@gmail.com>
2013-04-09 02:16:52 +1200

    Gitdive_autolog | MOOD:"tired" | COFFEE:none | LOCATION:"masters desk"

--- GitDive_Cinder/src/GitDataTypes.h
+++ GitDive_Cinder/src/GitDataTypes.h
@@ -31,7 +31,7 @@ public:
         elem = element;
         str = elemStr;
         position = pos;
-        setPosition(cinder::Vec2f((float)position * 10.0f, 0.0f);
+        setPosition(cinder::Vec2f((float)position * 10.0f, 0.0f));
     };
     string getStr(){return str;};
     string elem;
