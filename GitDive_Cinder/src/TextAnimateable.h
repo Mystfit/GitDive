b1d28bc24ca416e7537ab575e83666ef505b0ccb
Byron Mallett <byronated@gmail.com>
2013-04-08 21:53:10 +1200

    Gitdive_autolog | MOOD:"tired" | COFFEE:none | LOCATION:"masters desk"

--- GitDive_Cinder/src/TextAnimateable.h
+++ GitDive_Cinder/src/TextAnimateable.h
@@ -24,13 +24,13 @@ public:
     virtual void animIn( cinder::Timeline &timeline, cinder::Vec2<float> destVec, cinder::Color destColour )
     {
         timeline.apply( &m_colour, destColour, 1.0f, cinder::easeOutAtan( 20 ) );
-        timeline.apply( &m_position, destVec, 0.5f, cinder::easeOutAtan( 10 ) ).finishFn( std::bind( &TextAnimateable::onAnimIn, this ) );
+        //timeline.apply( &m_position, destVec, 0.5f, cinder::easeOutAtan( 10 ) ).finishFn( std::bind( &TextAnimateable::onAnimIn, this ) );
     }
     
     virtual void animOut( cinder::Timeline &timeline, cinder::Vec2<float> destVec, cinder::Color destColour )
     {
         timeline.apply( &m_colour, destColour, 1.0f, cinder::easeOutAtan( 20 ) );
-        timeline.apply( &m_position, destVec, 0.5f, cinder::easeOutAtan( 10 ) ).finishFn( std::bind( &TextAnimateable::onAnimIn, this ) );    }
+        //timeline.apply( &m_position, destVec, 0.5f, cinder::easeOutAtan( 10 ) ).finishFn( std::bind( &TextAnimateable::onAnimIn, this ) );    }
     
     virtual void onAnimIn();
     virtual void onAnimOut();
