07a68110e27fe6a4b540d5450e9fd9cc3af82371
Byron Mallett <byronated@gmail.com>
2013-04-08 21:51:43 +1200

    Gitdive_autolog | MOOD:"tired" | COFFEE:none | LOCATION:"masters desk"

--- GitDive_Cinder/src/TextAnimateable.h
+++ GitDive_Cinder/src/TextAnimateable.h
@@ -21,13 +21,13 @@ public:
     
     virtual std::string getStr();
     
-    virtual void animIn( cinder::Timeline &timeline, Vec2<float> destVec, cinder::Color destColour )
+    virtual void animIn( cinder::Timeline &timeline, cinder::Vec2<float> destVec, cinder::Color destColour )
     {
-        timeline.apply( &m_colour, destColour, 1.0f, EaseOutAtan( 20 ) );
+        timeline.apply( &m_colour, destColour, 1.0f, cinder::easeOutAtan( 20 ) );
         timeline.apply( &m_position, destVec, 0.5f, cinder::easeOutAtan( 10 ) ).finishFn( std::bind( &TextAnimateable::onAnimIn, this ) );
     }
     
-    virtual void animOut( cinder::Timeline &timeline, cinder::Vec2<float> destVec, Color destColour )
+    virtual void animOut( cinder::Timeline &timeline, cinder::Vec2<float> destVec, cinder::Color destColour )
     {
         timeline.apply( &m_colour, destColour, 1.0f, EaseOutAtan( 20 ) );
         timeline.apply( &m_position, destVec, 0.5f, cinder::easeOutAtan( 10 ) ).finishFn( std::bind( &TextAnimateable::onAnimIn, this ) );    }
@@ -36,8 +36,8 @@ public:
     virtual void onAnimOut();
 
 protected:
-    cinder::Anim<Vec2f> m_position;
-    Anim<cinder::Color> m_colour;    
+    cinder::Anim<cinder::Vec2f> m_position;
+    Anim<cinder::Color> m_colour;
 };
 
 #endif /* defined(__GitDive_Cinder__TextAnimateable__) */
