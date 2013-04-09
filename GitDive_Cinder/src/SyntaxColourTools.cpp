6be3eb5391ff7d439a66dcbe3c6b9f386ef34001
Byron Mallett <byronated@gmail.com>
2013-04-09 17:22:08 +1200

    Gitdive_autolog | MOOD:"cautious optimism" | COFFEE:none | LOCATION:"masters desk"

--- GitDive_Cinder/src/SyntaxColourTools.cpp
+++ GitDive_Cinder/src/SyntaxColourTools.cpp
@@ -88,7 +88,7 @@ void LineFormatter::format(const std::string &s, const srchilite::FormatterParam
     //Utils::strip(str, "\n");
     //std::cout << "Adding " << elem << " to line " << m_targetLine->getLinePos() << " at pos " << params->start << "|" << str << "|" << endl;
     LineElement elem1(elem, str, params->start);
-    elem.setColour( srchilite::  );
+    elem1.setColour( srchilite::  );
     m_targetLine->addLineElement(LineElement(elem, str, params->start));
 }
 
