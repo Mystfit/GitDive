87b36a4f16e9898dab9a676503f77ee1cfa9130e
Byron Mallett <byronated@gmail.com>
2013-04-09 17:41:33 +1200

    Gitdive_autolog | MOOD:"cautious optimism" | COFFEE:none | LOCATION:"masters desk"

--- GitDive_Cinder/src/SyntaxColourTools.cpp
+++ GitDive_Cinder/src/SyntaxColourTools.cpp
@@ -108,7 +108,7 @@ void LineFormatter::syntaxParseLines(vector<boost::shared_ptr<Line> > lines, str
     srchilite::RegexRuleFactory ruleFactory;
     srchilite::LangDefManager langDefManager(&ruleFactory);
     srchilite::SourceHighlighter highlighter(langDefManager.getHighlightState(DATADIR, lang));
-    srchilit::StyleFileParser styleParser;
+    srchilite::StyleFileParser styleParser;
     
     //Set up formatters to modify our lines
     boost::shared_ptr<LineFormatter> passthroughLineFormatter(new LineFormatter("", boost::shared_ptr<Line>()));
