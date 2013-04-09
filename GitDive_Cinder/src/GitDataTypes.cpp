11f299f2d3b981f81af0df99810f6a2ebfdb8e72
Byron Mallett <byronated@gmail.com>
2013-04-09 17:10:11 +1200

    Gitdive_autolog | MOOD:"cautious optimism" | COFFEE:none | LOCATION:"masters desk"

--- GitDive_Cinder/src/GitDataTypes.cpp
+++ GitDive_Cinder/src/GitDataTypes.cpp
@@ -64,7 +64,7 @@ string Line::getStr(){
 
 void Line::update(){
     for(std::vector<LineElement>::iterator it = m_lineElements.begin(); it != m_lineElements.end(); ++it){
-        if(!it->bIsFloating) it->setPosition( cinder::Vec2f((float)it->position * TextRenderer::FONT_WIDTH, getPosition().y) );
+        if(!it->bIsFloating) it->setPosition( cinder::Vec2f((float)it->position * 8.0f, getPosition().y) );
     }
 }
 
