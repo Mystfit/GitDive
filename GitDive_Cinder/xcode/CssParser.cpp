50d5d6ff6eb3d96d5d637cc6cd052758b38319db
Byron Mallett <byronated@gmail.com>
2013-04-09 21:11:17 +1200

    Gitdive_autolog | MOOD:"cautious optimism" | COFFEE:none | LOCATION:"masters desk"

---
+++ GitDive_Cinder/xcode/CssParser.cpp
@@ -0,0 +1,9 @@
+//
+//  CssParser.cpp
+//  GitDive_Cinder
+//
+//  Created by Byron Mallett on 9/04/13.
+//
+//
+
+#include "CssParser.h"
---
+++ GitDive_Cinder/xcode/CssParser.h
@@ -0,0 +1,14 @@
+//
+//  CssParser.h
+//  GitDive_Cinder
+//
+//  Created by Byron Mallett on 9/04/13.
+//
+//
+
+#ifndef __GitDive_Cinder__CssParser__
+#define __GitDive_Cinder__CssParser__
+
+#include <iostream>
+
+#endif /* defined(__GitDive_Cinder__CssParser__) */
