4e7e8213e67a7c1378549cb8c7b7008e296f84a8
Byron Mallett <byronated@gmail.com>
2013-04-09 22:03:21 +1200

    Gitdive_autolog | MOOD:"cautious optimism" | COFFEE:none | LOCATION:"masters desk"

--- GitDive_Cinder/src/TextRenderer.cpp
+++ GitDive_Cinder/src/TextRenderer.cpp
@@ -45,7 +45,7 @@ void TextRenderer::breakLine(boost::shared_ptr<Line> line){
         m_freeLineElements.push_back(lineElems[i]);
         m_freeLineElements.back().setColour(cinder::Color::hex(m_cssColours.getColour(m_freeLineElements.back().elem)  ) );
         m_freeLineElements.back().setFloating();
-        m_freeLineElements.back().animOut(m_timeline, m_freeLineElements.back().getPosition(), cinder::Color(0,0,0));
+        m_freeLineElements.back().animOut(m_timeline, m_freeLineElements.back().getPosition(), cinder::Color(0,0,0), 10.0f);
     }
     
     line.reset();
