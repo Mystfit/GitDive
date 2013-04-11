c9a99dee365fb7024e54726bd24bdc27324e395e
Byron Mallett <byronated@gmail.com>
2013-04-11 16:53:20 +1200

    Gitdive_autolog | MOOD:"cautious optimism" | COFFEE:none | LOCATION:"masters desk"

--- GitDive_Cinder/src/TextAnimateable.h
+++ GitDive_Cinder/src/TextAnimateable.h
@@ -9,7 +9,7 @@
 #ifndef __GitDive_Cinder__TextAnimateable__
 #define __GitDive_Cinder__TextAnimateable__
 
-#define MAX_LIFE = 1000
+#define MAX_LIFE 1000
 
 #include <iostream>
 #include "cinder/Timeline.h"
