4a87a4c49f66c9256a852ee3321726b1e1ec70c8
Byron Mallett <byronated@gmail.com>
2013-04-13 22:29:48 +1200

    Gitdive_autolog | MOOD:"cautious optimism" | COFFEE:none | LOCATION:"masters desk"

--- GitDive_Cinder/src/LineElementFactory.cpp
+++ GitDive_Cinder/src/LineElementFactory.cpp
@@ -18,7 +18,7 @@ boost::shared_ptr<LineElement> LineElementFactory::getFreeLineElement(string ele
     vector< boost::shared_ptr<LineElement> >::iterator elem;
     for(elem = m_freeLineElements.begin(); elem != m_freeLineElements.end(); ++elem){
         if(elem->get()->elem == elemType && elem->get()->getStr() == search){
-            lineElem = elem->get();
+            return elem;
         }
     }
     
