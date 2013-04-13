9018679f19dc61dce0af7abb58ba1fb7ef20dd63
Byron Mallett <byronated@gmail.com>
2013-04-13 22:19:55 +1200

    Gitdive_autolog | MOOD:"cautious optimism" | COFFEE:none | LOCATION:"masters desk"

---
+++ GitDive_Cinder/src/LineElementFactory.cpp
@@ -0,0 +1,9 @@
+//
+//  LineElementFactory.cpp
+//  GitDive_Cinder
+//
+//  Created by Byron Mallett on 13/04/13.
+//
+//
+
+#include "LineElementFactory.h"
---
+++ GitDive_Cinder/src/LineElementFactory.h
@@ -0,0 +1,14 @@
+//
+//  LineElementFactory.h
+//  GitDive_Cinder
+//
+//  Created by Byron Mallett on 13/04/13.
+//
+//
+
+#ifndef __GitDive_Cinder__LineElementFactory__
+#define __GitDive_Cinder__LineElementFactory__
+
+#include <iostream>
+
+#endif /* defined(__GitDive_Cinder__LineElementFactory__) */
