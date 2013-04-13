17b5b26dd8c1da161ff9789e639b4b0bf49da152
Byron Mallett <byronated@gmail.com>
2013-04-13 18:54:12 +1200

    Gitdive_autolog | MOOD:"cautious optimism" | COFFEE:none | LOCATION:"masters desk"

--- GitDive_Cinder/src/TextRenderer.cpp
+++ GitDive_Cinder/src/TextRenderer.cpp
@@ -16,7 +16,7 @@ TextRenderer::TextRenderer(){
 	m_textureFont = cinder::gl::TextureFont::create( customFont, f, supportedChars );
     
     string cssPath = DATADIR;
-    string cssFile = "sh_bipolar.css";
+    string cssFile = "sh_vim-dark.css";
     m_cssColours = boost::shared_ptr<CssParser>(new CssParser());
     m_cssColours->loadFile( cssPath + "/" + cssFile );
 }
