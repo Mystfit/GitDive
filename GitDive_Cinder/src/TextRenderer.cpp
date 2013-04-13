3ce5570e1ca2eadf66bf5ce5f417ed9d2755b406
Byron Mallett <byronated@gmail.com>
2013-04-13 23:21:26 +1200

    Gitdive_autolog | MOOD:"cautious optimism" | COFFEE:none | LOCATION:"masters desk"

--- GitDive_Cinder/src/TextRenderer.cpp
+++ GitDive_Cinder/src/TextRenderer.cpp
@@ -45,14 +45,9 @@ void TextRenderer::animLinesIn(vector<boost::shared_ptr<Line> > lines){
 
 void TextRenderer::breakLine(boost::shared_ptr<Line> line){
     vector< boost::shared_ptr<LineElement> > lineElems = line->getLineElements();
-    line->animOut(m_timeline, cinder::Vec2f(0, line->getLinePos() * LINE_HEIGHT), cinder::Color(0.2,0.2,0.2));
     
-    //Rescue line elements first
-    for(int i = 0; i < lineElems.size(); i++){
-        m_freeLineElements.push_back(lineElems[i]);
-        m_freeLineElements.back()->setFloating();
-        m_freeLineElements.back()->animOut(m_timeline, m_freeLineElements.back()->getPosition(), cinder::Color(0,0,0), 10.0f);
-    }
+    m_lineManager->salvageLine(line->getLineElements());
+    line->animOut(m_timeline, cinder::Vec2f(0, line->getLinePos() * LINE_HEIGHT), cinder::Color(0.2,0.2,0.2));
 
     line.reset();
 }
