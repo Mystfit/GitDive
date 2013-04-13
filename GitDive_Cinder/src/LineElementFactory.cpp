8d0c2b04748c5c2303197e2c50ff908b0a1c4c4b
Byron Mallett <byronated@gmail.com>
2013-04-13 22:38:27 +1200

    Gitdive_autolog | MOOD:"cautious optimism" | COFFEE:none | LOCATION:"masters desk"

--- GitDive_Cinder/src/LineElementFactory.cpp
+++ GitDive_Cinder/src/LineElementFactory.cpp
@@ -12,19 +12,21 @@ LineElementFactory::LineElementFactory(){
     
 }
 
-boost::shared_ptr<LineElement> LineElementFactory::getFreeLineElement(string elemType, string search){
+boost::shared_ptr<LineElement> LineElementFactory::getFreeLineElement(string elemType, string search, int index){
     boost::shared_ptr<LineElement> lineElem;
     
     vector< boost::shared_ptr<LineElement> >::iterator elem;
     for(elem = m_freeLineElements.begin(); elem != m_freeLineElements.end(); ++elem){
         if(elem->get()->elem == elemType && elem->get()->getStr() == search){
-            
-            std:
-            
+    
             lineElem = (boost::shared_ptr<LineElement>)elem->get();
-            
-            
+            m_freeLineElements.erase(elem);
+            break;
         }
+    }
+    
+    if(!lineElem){
+        lineElem = boost::shared_ptr<LineElement>(new LineElement(elemType, search, index));
     }
     
     
