fc8c4c050d3c1e094d8d5a369bed2ce4138dea24
Byron Mallett <byronated@gmail.com>
2013-04-15 03:08:24 +1200

    Gitdive_autolog | MOOD:"cautious optimism" | COFFEE:none | LOCATION:"masters desk"

--- GitDive_Cinder/src/SyntaxColourTools.cpp
+++ GitDive_Cinder/src/SyntaxColourTools.cpp
@@ -85,7 +85,7 @@ void LineFormatter::format(const std::string &s, const srchilite::FormatterParam
     string str = s;
     //std::cout << "Adding " << elem << " to line " << m_targetLine->getLinePos() << " at pos " << params->start << "|" << str << "|" << endl;
     
-    //Get a free element from the elemManager and let it handle creating new elements
+    //Get a free element from the elemManager and let it handle creating new elements for us
     boost::shared_ptr<LineElement> newElem = m_lineElemManager->getFreeLineElement(elem, str, params->start);
     
     m_targetLine->addLineElement(newElem);
