7adff6ae1fbcdec52236dc23eecbc3d25c3db081
Byron Mallett <byronated@gmail.com>
2013-04-15 04:48:43 +1200

    Gitdive_autolog | MOOD:"cautious optimism" | COFFEE:none | LOCATION:"masters desk"

--- GitDive_Cinder/src/LineElementManager.cpp
+++ GitDive_Cinder/src/LineElementManager.cpp
@@ -53,8 +53,10 @@ void LineElementManager::update(){
     //Remove old line elements hanging around
     for(vector< boost::shared_ptr<LineElement> >::iterator it = m_freeLineElements.begin(); it != m_freeLineElements.end(); ++it ){
         it->get()->update();
-        m_freeLineElements.erase( std::remove_if(m_freeLineElements.begin(), m_freeLineElements.end(), [](const myobj & o)
-        { return o.m_bMarkedDelete),
+        m_freeLineElements.erase(
+                                 std::remove_if(m_freeLineElements.begin(), m_freeLineElements.end(), [](const boost::shared_ptr<LineElement> o)
+                                                 { return o->markedForDeletion(); }
+                                                 ),
                      myList.end());
     }
     
