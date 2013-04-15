604be66e7eb4173d2a69ba6c2fd937adddf835cb
Byron Mallett <byronated@gmail.com>
2013-04-16 10:05:43 +1200

    Gitdive_autolog | MOOD:"cautious optimism" | COFFEE:none | LOCATION:"masters desk"

--- GitDive_Cinder/src/TextRenderer.cpp
+++ GitDive_Cinder/src/TextRenderer.cpp
@@ -149,7 +149,7 @@ void TextRenderer::update(){
             //Update camera
             if(bCameraFollowing){
                 cinder::Anim<cinder::Vec2f> camOffset = cinder::Vec2f(0.0f, line->get()->getLinePos() * LINE_HEIGHT);
-                m_timeline.apply(&m_textOffset, camOffset, 1.0f);
+                m_timeline->apply(&m_textOffset, camOffset, 1.0f);
                 
             }
             
