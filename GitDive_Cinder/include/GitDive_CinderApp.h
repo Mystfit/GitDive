a0a4689b660af8b3399bfc330dfe75b69a01d310
Byron Mallett <byronated@gmail.com>
2013-04-16 08:17:13 +1200

    Gitdive_autolog | MOOD:"cautious optimism" | COFFEE:none | LOCATION:"masters desk"

--- GitDive_Cinder/include/GitDive_CinderApp.h
+++ GitDive_Cinder/include/GitDive_CinderApp.h
@@ -43,6 +43,7 @@ public:
     void pauseVisualization(){ bVizActive = !bVizActive; };
     void jumpToEnd();
     void setTrackedFile();
+    void setViewedFile();
     
     //Managers
     GitFileManager fManager;
