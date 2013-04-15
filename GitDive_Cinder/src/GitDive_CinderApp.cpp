1423d3472a7e92d3c07eddc70cc2d0bfc93d398e
Byron Mallett <byronated@gmail.com>
2013-04-16 02:02:57 +1200

    Gitdive_autolog | MOOD:"cautious optimism" | COFFEE:none | LOCATION:"masters desk"

--- GitDive_Cinder/src/GitDive_CinderApp.cpp
+++ GitDive_Cinder/src/GitDive_CinderApp.cpp
@@ -110,8 +110,7 @@ void GitDive_CinderApp::jumpToEnd(){
 
 
 void GitDive_CinderApp::mouseWheel(MouseEvent event){
-    console() << event.getWheelIncrement() << endl;
-    tRender->setTextOffset(event.getWheelIncrement());
+    tRender->setTextOffset(event.getWheelIncrement() * 5);
 }
 
 
