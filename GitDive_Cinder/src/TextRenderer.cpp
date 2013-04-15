9b3c8029060bb298428cb3bc98a4c1158af32dee
Byron Mallett <byronated@gmail.com>
2013-04-16 03:29:31 +1200

    Gitdive_autolog | MOOD:"cautious optimism" | COFFEE:none | LOCATION:"masters desk"

--- GitDive_Cinder/src/TextRenderer.cpp
+++ GitDive_Cinder/src/TextRenderer.cpp
@@ -108,10 +108,10 @@ void TextRenderer::update(){
                 if(line->get()->getLineState() == Line::LINE_ADDED){
                     //line->get()->setColour(cinder::Color::hex(m_cssColours->getColour("normal")));
                     //line->get()->setPosition(cinder::Vec2f(0, line->get()->getLinePos() * LINE_HEIGHT));
-                    //line->get()->animIn(m_timeline, cinder::Vec2f(0, line->get()->getLinePos() * LINE_HEIGHT), cinder::Color(1.0f, 1.0f, 1.0f), 1.0f);
+                    line->get()->animIn(m_timeline, cinder::Vec2f(0, line->get()->getLinePos() * LINE_HEIGHT), cinder::Color(1.0f, 1.0f, 1.0f), 1.0f);
                 } else if(line->get()->getLineState() == Line::LINE_NORMAL){
                     //line->get()->setPosition(cinder::Vec2f(0, line->get()->getLinePos() * LINE_HEIGHT));
-                    //line->get()->animIn(m_timeline, cinder::Vec2f(0, line->get()->getLinePos() * LINE_HEIGHT), cinder::Color(1.0f, 1.0f, 1.0f), 1.0f);
+                    line->get()->animIn(m_timeline, cinder::Vec2f(0, line->get()->getLinePos() * LINE_HEIGHT), cinder::Color(1.0f, 1.0f, 1.0f), 1.0f);
                 }
             
             
