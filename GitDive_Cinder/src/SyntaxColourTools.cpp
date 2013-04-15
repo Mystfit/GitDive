917540ceb28c9502041cbee87da827f47f608960
Byron Mallett <byronated@gmail.com>
2013-04-15 19:00:24 +1200

    Gitdive_autolog | MOOD:"cautious optimism" | COFFEE:none | LOCATION:"masters desk"

--- GitDive_Cinder/src/SyntaxColourTools.cpp
+++ GitDive_Cinder/src/SyntaxColourTools.cpp
@@ -86,7 +86,9 @@ void LineFormatter::format(const std::string &s, const srchilite::FormatterParam
     
     //Get a free element from the elemManager and let it handle creating elements for us (I swear this is such a bloody hack)
     boost::shared_ptr<LineElement> newElem = m_lineElemManager->getFreeLineElement(elem, str, params->start);
-    
+    if(!m_targetLine){
+        cout << "WHY IS THERE NO LINE" << endl;
+    }
     m_targetLine->addLineElement(newElem);
 }
 
