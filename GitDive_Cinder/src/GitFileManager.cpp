9377e674d2904753edd12185832a1f5de001ce70
Byron Mallett <byronated@gmail.com>
2013-04-15 22:37:59 +1200

    Gitdive_autolog | MOOD:"cautious optimism" | COFFEE:none | LOCATION:"masters desk"

--- GitDive_Cinder/src/GitFileManager.cpp
+++ GitDive_Cinder/src/GitFileManager.cpp
@@ -112,6 +112,7 @@ void GitFileManager::applyDiffToFile(GitFile &file, boost::shared_ptr<Diff> diff
                 //Erase the line from the file
                 file.getLines()[i]->markForRemoval();
                 
+                //Only break lin
                 if(file.getFilename().size()){
                     if(m_tRender->getTargetFile()){
                         if(file.getFilename() == m_tRender->getTargetFile()->getFilename() ) m_tRender->breakLine(file.getLines()[i]);
