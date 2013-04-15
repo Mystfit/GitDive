d2994888525e79a0302325037f56015ae50062e8
Byron Mallett <byronated@gmail.com>
2013-04-15 16:00:37 +1200

    Gitdive_autolog | MOOD:"cautious optimism" | COFFEE:none | LOCATION:"masters desk"

--- GitDive_Cinder/src/LineElementManager.cpp
+++ GitDive_Cinder/src/LineElementManager.cpp
@@ -57,9 +57,8 @@ void LineElementManager::update(){
     
     m_freeLineElements.erase(
                  std::remove_if(m_freeLineElements.begin(), m_freeLineElements.end(),
-                                [](const boost::shared_ptr<LineElement> o) {
-                                    bool state = false;
-                                    return o->markedForDeletion();
+                                [](const boost::shared_ptr<LineElement> elem) {
+                                    return (elem->markedForDeletion() && elem->isFloating);
                                 }),
                  m_freeLineElements.end());
     
