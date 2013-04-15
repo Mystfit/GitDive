76b327a5bc42cb0a88861659bcb1a0763c85bad8
Byron Mallett <byronated@gmail.com>
2013-04-16 05:16:49 +1200

    Gitdive_autolog | MOOD:"cautious optimism" | COFFEE:none | LOCATION:"masters desk"

--- GitDive_Cinder/src/TextRenderer.cpp
+++ GitDive_Cinder/src/TextRenderer.cpp
@@ -154,7 +154,7 @@ void TextRenderer::update(){
 void TextRenderer::draw(){
     
     cinder::gl::pushMatrices();
-    cinder::gl
+    loadIde
     vector< boost::shared_ptr<Line> > lines;
     
     if(m_targetFile) lines = m_targetFile->getLines();
