b93c6d893ebc75c4d8e067a3f24b0c8398fb2dea
Byron Mallett <byronated@gmail.com>
2013-04-15 22:18:57 +1200

    Gitdive_autolog | MOOD:"cautious optimism" | COFFEE:none | LOCATION:"masters desk"

--- GitDive_Cinder/src/TextRenderer.cpp
+++ GitDive_Cinder/src/TextRenderer.cpp
@@ -107,7 +107,8 @@ void TextRenderer::update(){
                 
                 if(elem->get()->isJustCreated()){
                     //Replace with something better
-                    elem->get()->animIn(m_timeline, cinder::Vec2f(elemX, lineY), cinder::Color::hex(m_cssColours->getColour(elem->get()->elem)), 5.0f);
+                    elem->get()->setPosition(cinder::Vec2f(elemX, lineY));
+                    //elem->get()->animIn(m_timeline, cinder::Vec2f(elemX, lineY), cinder::Color::hex(m_cssColours->getColour(elem->get()->elem)), 5.0f);
                 }
             }
         }
