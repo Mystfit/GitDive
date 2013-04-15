0d6ea8c6185a24044a14c7b542a1036605981386
Byron Mallett <byronated@gmail.com>
2013-04-16 02:56:27 +1200

    Gitdive_autolog | MOOD:"cautious optimism" | COFFEE:none | LOCATION:"masters desk"

--- GitDive_Cinder/src/TextRenderer.cpp
+++ GitDive_Cinder/src/TextRenderer.cpp
@@ -127,7 +127,6 @@ void TextRenderer::update(){
                         //elem->get()->setColour(cinder::Color::hex(m_cssColours->getColour(elem->get()->elem)));
                         //elem->get()->setPosition(cinder::Vec2f(elemX, lineY));
                         float randOffset = ((float)rand()/(float)RAND_MAX) * 0.5f;
-                        cout << randOffset << endl;
                         elem->get()->animIn(m_timeline, cinder::Vec2f(elemX, lineY), cinder::Color::hex(m_cssColours->getColour(elem->get()->elem)), 0.5f );
                     }
                 }
