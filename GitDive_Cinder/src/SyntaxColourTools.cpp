2c4db111118b8385d848126a6df55c93ca09d6f4
Byron Mallett <byronated@gmail.com>
2013-04-15 19:14:23 +1200

    Gitdive_autolog | MOOD:"cautious optimism" | COFFEE:none | LOCATION:"masters desk"

--- GitDive_Cinder/src/SyntaxColourTools.cpp
+++ GitDive_Cinder/src/SyntaxColourTools.cpp
@@ -104,7 +104,7 @@ void LineFormatterManager::syntaxParseLines(vector<boost::shared_ptr<Line> > lin
     for(int i = 0; i < lines.size(); i++){
         if(lines[i]->getLineState() == Line::LINE_ADDED){
             params.start = 0;
-            formatterManager->setTargetLine(lines[i]);
+            setTargetLine(lines[i]);
             highlighter.highlightParagraph(lines[i]->getStr());
         }
     }
