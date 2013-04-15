f8c5e96f6825a0b00f35eb51475bfe5e187eae19
Byron Mallett <byronated@gmail.com>
2013-04-16 01:43:01 +1200

    Gitdive_autolog | MOOD:"cautious optimism" | COFFEE:none | LOCATION:"masters desk"

--- GitDive_Cinder/src/TextRenderer.h
+++ GitDive_Cinder/src/TextRenderer.h
@@ -41,7 +41,7 @@ public:
     void setTimeline(cinder::TimelineRef timelineRef){ m_timeline = timelineRef; };
     
     //Sets the position of the file on the screen
-    void setTextOffset(float ){m_textOffset = position;};
+    void setTextOffset(float increment);
     void setScrollVelocity(cinder::Vec2f velocity){m_scrollVelocity = velocity; };
     void setTextZoom(float zoom){m_textZoom = zoom; };
     
