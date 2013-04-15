3073cb29746bb02310e912e98a44b56263473355
Byron Mallett <byronated@gmail.com>
2013-04-16 09:00:15 +1200

    Gitdive_autolog | MOOD:"cautious optimism" | COFFEE:none | LOCATION:"masters desk"

--- GitDive_Cinder/include/GitDive_CinderApp.h
+++ GitDive_Cinder/include/GitDive_CinderApp.h
@@ -34,7 +34,7 @@ public:
     void mouseUp(MouseEvent event);
     void mouseWheel(MouseEvent event);
     void keyDown( KeyEvent event );
-    void keyUp(KeyEvent event){
+    void keyUp(KeyEvent event);
 	void update();
 	void draw();
     void resetView();
