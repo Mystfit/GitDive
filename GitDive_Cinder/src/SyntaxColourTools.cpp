f492ef5d477addf1ea452c59d9f685dc2e9ce3db
Byron Mallett <byronated@gmail.com>
2013-04-15 18:51:33 +1200

    Gitdive_autolog | MOOD:"cautious optimism" | COFFEE:none | LOCATION:"masters desk"

--- GitDive_Cinder/src/SyntaxColourTools.cpp
+++ GitDive_Cinder/src/SyntaxColourTools.cpp
@@ -87,7 +87,7 @@ void LineFormatter::format(const std::string &s, const srchilite::FormatterParam
     //Get a free element from the elemManager and let it handle creating elements for us (I swear this is such a bloody hack)
     boost::shared_ptr<LineElement> newElem = m_lineElemManager->getFreeLineElement(elem, str, params->start);
     
-    if(s == "Diff"){
+    if(s == "namespace"){
         cout << "Where is this going? " << s << endl;
     }
 
