361367650ee15c5912d96b89886864e0d4abd263
Byron Mallett <byronated@gmail.com>
2013-04-15 20:40:41 +1200

    Gitdive_autolog | MOOD:"cautious optimism" | COFFEE:none | LOCATION:"masters desk"

--- GitDive_Cinder/src/GitDataTypes.cpp
+++ GitDive_Cinder/src/GitDataTypes.cpp
@@ -45,6 +45,8 @@ Line::Line(string lineStr, bool justUpdated){
 }
 
 void Line::setPosition(cinder::Vec2f position){
+    TextAnimateable::setPosition(position);
+    
     for(vector< boost::shared_ptr<LineElement> >::iterator elem = lineElems.begin(); elem != lineElems.end(); ++elem){
         elem->get()->setPosition(cinder::Vec2f(position.x + (float)elem->get()->position * FONT_WIDTH, position.y));
     }
