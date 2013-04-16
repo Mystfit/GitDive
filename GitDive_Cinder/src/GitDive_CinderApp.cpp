5b6050de369dbdfc2478e1c7ec3fff3baaecfe92
Byron Mallett <byronated@gmail.com>
2013-04-16 12:38:03 +1200

    Gitdive_autolog | MOOD:"cautious optimism" | COFFEE:none | LOCATION:"masters desk"

--- GitDive_Cinder/src/GitDive_CinderApp.cpp
+++ GitDive_Cinder/src/GitDive_CinderApp.cpp
@@ -124,6 +124,7 @@ void GitDive_CinderApp::repoPathDialog(){
 
 void GitDive_CinderApp::trackedFileDialog(){
     m_targetTrackedFile = cinder::app::getOpenFilePath().string();
+    m_targetViewFile = m_targetTrackedFile;
     
     //Remove repo path from front of file path
     m_targetTrackedFile.erase(0, m_repoPath.size()+1);
