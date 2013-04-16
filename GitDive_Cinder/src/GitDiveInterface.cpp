fb33084457bc114b7564cfaaa60250597343a14f
Byron Mallett <byronated@gmail.com>
2013-04-16 13:23:21 +1200

    Gitdive_autolog | MOOD:"cautious optimism" | COFFEE:none | LOCATION:"masters desk"

---
+++ GitDive_Cinder/src/GitDiveInterface.cpp
@@ -0,0 +1,9 @@
+//
+//  GitDiveInterface.cpp
+//  GitDive_Cinder
+//
+//  Created by Byron Mallett on 16/04/13.
+//
+//
+
+#include "GitDiveInterface.h"
---
+++ GitDive_Cinder/src/GitDiveInterface.h
@@ -0,0 +1,14 @@
+//
+//  GitDiveInterface.h
+//  GitDive_Cinder
+//
+//  Created by Byron Mallett on 16/04/13.
+//
+//
+
+#ifndef __GitDive_Cinder__GitDiveInterface__
+#define __GitDive_Cinder__GitDiveInterface__
+
+#include <iostream>
+
+#endif /* defined(__GitDive_Cinder__GitDiveInterface__) */
