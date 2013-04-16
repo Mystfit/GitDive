f64de8908e54bf58392b348b62baabf1ba844562
Byron Mallett <byronated@gmail.com>
2013-04-16 13:13:49 +1200

    Gitdive_autolog | MOOD:"cautious optimism" | COFFEE:none | LOCATION:"masters desk"

--- GitDive_Cinder/src/GitDive_CinderApp.cpp
+++ GitDive_Cinder/src/GitDive_CinderApp.cpp
@@ -50,7 +50,7 @@ void GitDive_CinderApp::setup()
 void GitDive_CinderApp::initUI(){
     //Setup UI
     mParams = params::InterfaceGl( "GitDive Options", Vec2i( 400, 350 ) );
-    mParams.setOptions("", "position=" );
+    mParams.setOptions("", "position='624 50'" );
     
     mParams.addButton("Open repository", std::bind(&GitDive_CinderApp::repoPathDialog, this));
     mParams.addParam("Repository path", &m_repoPath);
