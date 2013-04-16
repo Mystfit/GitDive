bffd3df251911923f2cf628de5981745f825f381
Byron Mallett <byronated@gmail.com>
2013-04-16 13:29:49 +1200

    Gitdive_autolog | MOOD:"cautious optimism" | COFFEE:none | LOCATION:"masters desk"

--- GitDive_Cinder/src/GitDiveInterface.h
+++ GitDive_Cinder/src/GitDiveInterface.h
@@ -13,7 +13,6 @@
 #include "cinder/params/Params.h"
 
 class GitDiveInterface : public cinder::params::InterfaceGl {
-	GitDiveInterface( const std::string &titleStr, const cinder::Vec2i &size);
 };
 
 #endif /* defined(__GitDive_Cinder__GitDiveInterface__) */
