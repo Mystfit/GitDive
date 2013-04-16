8e7b66643d32e10133c97445881edb7833fab9b9
Byron Mallett <byronated@gmail.com>
2013-04-16 12:47:40 +1200

    Gitdive_autolog | MOOD:"cautious optimism" | COFFEE:none | LOCATION:"masters desk"

--- GitDive_Cinder/src/TextRenderer.cpp
+++ GitDive_Cinder/src/TextRenderer.cpp
@@ -48,6 +48,7 @@ void TextRenderer::setLinesAnimatable(){
 
 void TextRenderer::setTextOffset(float increment){
     m_textOffset.ptr()->y += increment;
+    m_textOffset.ptr()->x = 
 //    if(m_targetFile){
 //        if(m_textOffset.y < floor(m_targetFile->getLines().size() * LINE_HEIGHT )){
 //            m_textOffset.y = floor(m_targetFile->getLines().size() * LINE_HEIGHT);
